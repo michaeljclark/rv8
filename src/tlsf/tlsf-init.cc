7ff98772b6261a6c098082af6b618ad37e92e0a8
Michael Clark <michael@metaparadigm.com>
2016-07-16 18:27:44 +1200

    Use malloc hooks for TLSF in emulator when compiling with GLIBC
    - When static linking, defining malloc, realloc and free causes
      duplicate symbols so we need to use the GLIB malloc hooks.

--- src/tlsf/tlsf-init.cc
+++ src/tlsf/tlsf-init.cc
@@ -10,34 +10,81 @@
 
 #include "tlsf.h"
 
+#define RISCV_TLSF_OVERRIDE_MALLOC
+#define RISCV_TLSF_USE_GLIBC_MALLOC_HOOK
+
+#if defined (RISCV_TLSF_OVERRIDE_MALLOC)
+
+#if defined (__GLIBC__) && defined (RISCV_TLSF_USE_GLIBC_MALLOC_HOOK)
+#include <malloc.h>
+#else
+#endif
+
 static const uintptr_t heap_loc = 0x780000000000; // 120 TiB
 static const size_t heap_size   = 0x10000000;     // 1GiB
-static void *heap_map           = NULL;
-static tlsf_t heap_tlsf         = NULL;
+static void *tlsf_heap_mmap     = nullptr;
+static tlsf_t tlsf_heap         = nullptr;
 
-void *malloc(size_t size)
+static void tlsf_initialize(void)
 {
-	if (heap_map == NULL) {
-		heap_map = mmap((void*)heap_loc, heap_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_FIXED | MAP_SHARED, -1, 0);
-		if (heap_map == MAP_FAILED) {
-			perror("mmap");
-			exit(9);
-		}
-		heap_tlsf = tlsf_create_with_pool(heap_map, heap_size);
+	tlsf_heap_mmap = mmap((void*)heap_loc, heap_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_FIXED | MAP_SHARED, -1, 0);
+	if (tlsf_heap_mmap == MAP_FAILED) {
+		perror("mmap");
+		exit(9);
 	}
-	return tlsf_malloc(heap_tlsf, size);
+	tlsf_heap = tlsf_create_with_pool(tlsf_heap_mmap, heap_size);
 }
 
-void free(void *ptr)
+#if defined (__GLIBC__) && defined (RISCV_TLSF_USE_GLIBC_MALLOC_HOOK)
+
+static void* tlsf_malloc_hook(size_t size, const void *caller)
 {
-	tlsf_free(heap_tlsf, ptr);
+	return tlsf_malloc(tlsf_heap, size);
 }
 
-void* operator new(std::size_t sz) {
-    return malloc(sz);
-}
-
-void operator delete(void* ptr) noexcept
+static void* tlsf_realloc_hook(void *ptr, size_t size, const void *caller)
 {
-    free(ptr);
+	return tlsf_realloc(tlsf_heap, ptr, size);
 }
+
+static void tlsf_free_hook(void *ptr, const void *caller)
+{
+	tlsf_free(tlsf_heap, ptr);
+}
+
+static void tlsf_initialize_hook(void)
+{
+	tlsf_initialize();
+	__malloc_hook = tlsf_malloc_hook;
+	__realloc_hook = tlsf_realloc_hook;
+	__free_hook = tlsf_free_hook;
+}
+
+void (*__MALLOC_HOOK_VOLATILE __malloc_initialize_hook) (void) = tlsf_initialize_hook;
+
+#else
+
+extern "C" void *malloc(size_t size)
+{
+	if (tlsf_heap_mmap == nullptr) tlsf_initialize();
+	return tlsf_malloc(tlsf_heap, size);
+}
+
+extern "C" void *realoc(void *ptr, size_t size)
+{
+	return tlsf_realloc(tlsf_heap, ptr, size);
+}
+
+extern "C" void free(void *ptr)
+{
+	tlsf_free(tlsf_heap, ptr);
+}
+
+#endif
+
+void* operator new(std::size_t sz) { return std::malloc(sz); }
+void* operator new[](std::size_t sz) { return std::malloc(sz); }
+void operator delete(void* ptr) noexcept { std::free(ptr); }
+void operator delete[](void* ptr) noexcept { std::free(ptr); }
+
+#endif
