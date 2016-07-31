df6efa5ea4f9df676dc527239da3edc8a889ee6a
Michael Clark <michael@metaparadigm.com>
2016-07-31 13:15:30 +1200

    Add structure for address space tagged TLB and tagged cache
    - Add address space tagged TLB and physically tagged, virtually
      indexed cache. Cache supports variable sized cache lines.
      Note: MMU backend and page walker that sits behind the caches
      still needs to be implemented.
    
    - Add address space tagged PPN struct for RV32/64
    - Add address space tagged VA to PPN struct RV32/64
    
      34-bit PA space on RV32 (10-bit ASID, 22-bit PPN)
      50-bit PA space on RV64 (26-bit ASID, 38-bit PPN)
    
    - Note: cache state is encoded in the uppermost 2 bits of PPN
      reducing the PA space to 32-bit on RV32 and 48-bit on RV64

--- app/riscv-test-mmu.cc
+++ app/riscv-test-mmu.cc
@@ -1,6 +1,8 @@
 //
-//  riscv-test-emulate.cc
+//  riscv-test-mmu.cc
 //
+
+#undef NDEBUG
 
 #include <cstdio>
 #include <cstring>
@@ -12,7 +14,10 @@
 
 #include "riscv-types.h"
 #include "riscv-endian.h"
+#include "riscv-bits.h"
+#include "riscv-machine.h"
 #include "riscv-mmu.h"
+#include "riscv-cache.h"
 
 using namespace riscv;
 
@@ -32,4 +37,31 @@ int main(int argc, char *argv[])
 	assert(sizeof(sv48_va) == 8);
 	assert(sizeof(sv48_pa) == 8);
 	assert(sizeof(sv48_pte) == 8);
+
+	typedef riscv::rv64_as_tagged_cache<65536,8,64> rv_cache_type;
+	printf("rv_cache_type::size              : %tu\n", rv_cache_type::size);
+	printf("rv_cache_type::key_size          : %tu\n", rv_cache_type::key_size);
+	printf("rv_cache_type::total_size        : %tu\n", rv_cache_type::total_size);
+	printf("rv_cache_type::num_entries       : %tu\n", rv_cache_type::num_entries);
+	printf("rv_cache_type::num_ways          : %tu\n", rv_cache_type::num_ways);
+	printf("rv_cache_type::line_size         : %tu\n", rv_cache_type::line_size);
+	printf("rv_cache_type::num_entries_shift : %tu\n", rv_cache_type::num_entries_shift);
+	printf("rv_cache_type::cache_line_shift  : %tu\n", rv_cache_type::cache_line_shift);
+	printf("rv_cache_type::num_ways_shift    : %tu\n", rv_cache_type::num_ways_shift);
+	printf("rv_cache_type::asid_bits         : %tu\n", rv_cache_type::asid_bits);
+	printf("rv_cache_type::ppn_bits          : %tu\n", rv_cache_type::ppn_bits);
+	printf("rv_cache_type::cache_line_mask   : 0x%08tx\n", rv_cache_type::cache_line_mask);
+	printf("rv_cache_type::num_entries_mask  : 0x%08tx\n", rv_cache_type::num_entries_mask);
+	printf("rv_cache_type::ppn_state_mask    : 0x%08tx\n", rv_cache_type::ppn_state_mask);
+
+	typedef riscv::rv64_as_tagged_tlb<128> rv_tlb_type;
+	printf("rv_tlb_type::size                : %tu\n", rv_tlb_type::size);
+	printf("rv_tlb_type::key_size            : %tu\n", rv_tlb_type::key_size);
+	printf("rv_tlb_type::mask                : 0x%08tx\n", rv_tlb_type::mask);
+
+	rv_tlb_type tlb;
+	tlb.insert_tag(/* va */ 0x10000, /* asid */0, /* ppn */ 1);
+	auto ent = tlb.lookup_tag(0x10000, 0);
+	assert(ent != nullptr);
+	assert(ent->ppn == 1);
 }
---
+++ src/emulator/riscv-cache.h
@@ -0,0 +1,197 @@
+//
+//  riscv-cache.h
+//
+
+#ifndef riscv_cache_h
+#define riscv_cache_h
+
+namespace riscv {
+
+	/* address space tagged physical page number */
+
+	template <typename UX, const int ASID_BITS, const int PPN_BITS>
+	struct as_tagged_ppn
+	{
+		static_assert(ASID_BITS + PPN_BITS == 32 || ASID_BITS + PPN_BITS == 64 ||
+			ASID_BITS + PPN_BITS == 128, "ASID_BITS + PPN_BITS == (32, 64, 128)");
+
+		enum : UX {
+			asid_bits = ASID_BITS,
+			ppn_bits = PPN_BITS
+		};
+
+		UX ppn  : PPN_BITS;
+		UX asid : ASID_BITS;
+	};
+
+	using rv32_as_tagged_ppn = as_tagged_ppn<u32,10,22>;
+	using rv64_as_tagged_ppn = as_tagged_ppn<u64,26,38>;
+
+
+	/* address space tagged virtual to physical mapping */
+
+	template <typename UX, const int ASID_BITS, const int PPN_BITS>
+	struct as_tagged_va_ppn: as_tagged_ppn<UX,ASID_BITS,PPN_BITS>
+	{
+		enum : UX {
+			asid_bits = ASID_BITS,
+			ppn_bits = PPN_BITS
+		};
+
+		UX va;
+	};
+
+	using rv32_as_tagged_va_ppn = as_tagged_va_ppn<u32,10,22>;
+	using rv64_as_tagged_va_ppn = as_tagged_va_ppn<u64,26,38>;
+
+
+	/* address space tagged tlb */
+
+	template <const size_t tlb_size, typename UX, typename AST_PT_VA>
+	struct as_tagged_tlb
+	{
+	    static_assert(ispow2(tlb_size), "tlb_size must be a power of 2");
+
+	    typedef AST_PT_VA as_tagged_va_ppn_type;
+
+		enum : UX {
+			size = tlb_size,
+			shift = ctz_pow2(size),
+			mask = (1ULL << shift) - 1,
+			key_size = sizeof(AST_PT_VA),
+		};
+
+		as_tagged_va_ppn_type tlb[size];
+
+		as_tagged_tlb() { flush(); }
+
+		void flush()
+		{
+			for (size_t i = 0; i < size; i++) {
+				tlb[i].asid = tlb[i].ppn = tlb[i].va = UX(-1);
+			}
+		}
+
+		void flush(UX asid)
+		{
+			for (size_t i = 0; i < size; i++) {
+				if (tlb[i].asid != asid) continue;
+				tlb[i].asid = tlb[i].ppn = tlb[i].va = UX(-1);
+			}
+		}
+
+		as_tagged_va_ppn_type* lookup_tag(UX vaddr, UX asid)
+		{
+			UX va = vaddr & page_mask;
+			for (size_t i = 0; i < size; i++) {
+				if (tlb[i].va == va && tlb[i].asid == asid) {
+					return &tlb[i];
+				}
+			}
+			return nullptr;
+		}
+
+		void insert_tag(UX vaddr, UX asid, UX ppn)
+		{
+			UX va = vaddr & page_mask;
+			size_t i;
+			for (i = 0; i < size; i++) {
+				if (tlb[i].va == UX(-1)) break;
+			}
+			if (i == size) {
+				i = (vaddr % 8191) & mask;
+			}
+			tlb[i].va = va;
+			tlb[i].asid = asid;
+			tlb[i].ppn = ppn;
+		}
+	};
+
+	template <const size_t tlb_entries> using rv32_as_tagged_tlb = as_tagged_tlb<tlb_entries,u32,rv32_as_tagged_va_ppn>;
+	template <const size_t tlb_entries> using rv64_as_tagged_tlb = as_tagged_tlb<tlb_entries,u64,rv64_as_tagged_va_ppn>;
+
+
+	/* address space and physically tagged, virtually indexed cache */
+
+	template <typename UX, typename AST_PT_VA, const size_t cache_size, const size_t cache_ways, const size_t cache_line_size>
+	struct as_tagged_cache
+	{
+	    static_assert(ispow2(cache_size), "cache_size must be a power of 2");
+	    static_assert(ispow2(cache_ways), "cache_ways must be a power of 2");
+	    static_assert(ispow2(cache_line_size), "cache_line_size must be a power of 2");
+
+	    typedef AST_PT_VA as_tagged_va_ppn_type;
+
+		enum : UX {
+			size =                cache_size,
+			line_size =           cache_line_size,
+			num_ways =            cache_ways,
+			num_entries =         size / (num_ways * line_size),
+			key_size =            sizeof(AST_PT_VA),
+			total_size =          cache_size + sizeof(AST_PT_VA) * num_entries * num_ways,
+
+			num_entries_shift =   ctz_pow2(num_entries),
+			cache_line_shift =    ctz_pow2(cache_line_size),
+			num_ways_shift =      ctz_pow2(num_ways),
+
+			cache_line_mask =   ~((UX(1) << cache_line_shift) - 1),
+			num_entries_mask =    (UX(1) << num_entries_shift) - 1,
+
+			asid_bits =           AST_PT_VA::asid_bits,
+			ppn_bits =            AST_PT_VA::ppn_bits,
+
+			/* cache state (encode in uppermost 2 bits of ppn) */
+			ppn_state_modified =  UX(0) << (ppn_bits - 2), /* modified */
+			ppn_state_exclusive = UX(1) << (ppn_bits - 2), /* exclusive */
+			ppn_state_shared =    UX(2) << (ppn_bits - 2), /* shared */
+			ppn_state_invalid =   UX(3) << (ppn_bits - 2), /* invalid */
+			ppn_state_mask    =   UX(3) << (ppn_bits - 2),
+		};
+
+		as_tagged_va_ppn_type cache_key[num_entries * num_ways];
+		u8 cache_data[num_entries * num_ways * cache_line_size];
+
+		as_tagged_cache() { flush(); }
+
+		void flush()
+		{
+			for (size_t i = 0; i < num_entries * num_ways; i++) {
+				cache_key[i].ppn = ppn_state_invalid;
+			}
+		}
+
+		void flush(UX asid)
+		{
+			for (size_t i = 0; i < num_entries * num_ways; i++) {
+				if (cache_key[i].asid != asid) continue;
+				cache_key[i].ppn = ppn_state_invalid;
+			}
+		}
+
+		u8* get_cache_line(UX vaddr, UX ppn, UX asid)
+		{
+			UX va = vaddr & cache_line_mask;
+			UX entry = vaddr & num_entries_mask;
+			AST_PT_VA *ent = cache_key + (entry << num_ways_shift);
+			for (size_t i = 0; i < num_ways; i++) {
+				if (ent->va == va && ent->ppn == ppn && ent->asid == asid) {
+					return cache_data + entry << cache_line_shift +
+						i << (cache_line_shift + num_ways_shift);
+				}
+				ent++;
+			}
+
+			// TODO - choose way to flush and fetch this line from memory
+
+			return nullptr;
+		}
+	};
+
+	template <const size_t cache_size, const size_t cache_ways, const size_t cache_line_size>
+	using rv32_as_tagged_cache = as_tagged_cache<u32,rv32_as_tagged_va_ppn,cache_size,cache_ways,cache_line_size>;
+
+	template <const size_t cache_size, const size_t cache_ways, const size_t cache_line_size>
+	using rv64_as_tagged_cache = as_tagged_cache<u64,rv64_as_tagged_va_ppn,cache_size,cache_ways,cache_line_size>;
+}
+
+#endif
