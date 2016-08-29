72ce09c24ed32192b142b8526bb29ed6198c239f
Michael Clark <michael@metaparadigm.com>
2016-08-30 01:45:22 +1200

    Implement unsigned mult for higher width {hi, lo} pair types
    - Added pair types _u16, _u32, _u64, _u128 for testing
    - Implemented splitting of product into hi and lo
    - Added carry positions to ASCII art diagram
    - Implemented manual carry propagation
    - Vary width of test_mult output based on operand size
    - Remove temporary verbose debugging messages
    - Added unsigned __int128 test case (test only works on
      compilers with support for __int128 and printf format
      for check value will display truncated u64 value)
    
    NOTE: check result currently require higher width host types.
    The check results will eventually be replaced with computed
    pair values in test assertions as the intention is the host
    does not need to support the higher width type.

--- app/riscv-test-mult.cc
+++ app/riscv-test-mult.cc
@@ -12,15 +12,100 @@
 
 using namespace riscv;
 
-enum test_print_mode
+struct _u16
 {
-	test_print_all,
-	test_print_fail,
-	test_print_debug
+	typedef u8 utype;
+
+	union {
+		u8                          b[2];
+	#if _BYTE_ORDER == _LITTLE_ENDIAN
+		struct { u8 lo;  u8 hi; }   p;
+	#else
+		struct { u8 hi;  u8 lo; }   p;
+	#endif
+	} val;
+
+	inline _u16() : val{ .b = { 0 } } {}
+	inline _u16(const _u16 &o) : val{ .p = o.val.p } {}
+
+	#if _BYTE_ORDER == _LITTLE_ENDIAN
+	inline _u16(u8 hi, u8 lo) : val{ .p = { .lo = lo, .hi = hi } } {}
+	#else
+	inline _u16(u8 hi, u8 lo) : val{ .p = { .hi = hi, .lo = lo } } {}
+	#endif
 };
 
-template <typename R, typename T, const bool debug = false>
-R mult(T x, T y)
+struct _u32
+{
+	typedef u16 utype;
+
+	union {
+		u8                          b[4];
+	#if _BYTE_ORDER == _LITTLE_ENDIAN
+		struct { u16 lo; u16 hi; }  p;
+	#else
+		struct { u16 hi; u16 lo; }  p;
+	#endif
+	} val;
+
+	inline _u32() : val{ .b = { 0 } } {}
+	inline _u32(const _u32 &o) : val{ .p = o.val.p } {}
+
+	#if _BYTE_ORDER == _LITTLE_ENDIAN
+	inline _u32(u16 hi, u16 lo) : val{ .p = { .lo = lo, .hi = hi } } {}
+	#else
+	inline _u32(u16 hi, u16 lo) : val{ .p = { .hi = hi, .lo = lo } } {}
+	#endif
+};
+
+struct _u64
+{
+	typedef u32 utype;
+
+	union {
+		u8                          b[8];
+	#if _BYTE_ORDER == _LITTLE_ENDIAN
+		struct { u32 lo; u32 hi; }  p;
+	#else
+		struct { u32 hi; u32 lo; }  p;
+	#endif
+	} val;
+
+	inline _u64() : val{ .b = { 0 } } {}
+	inline _u64(const _u64 &o) : val{ .p = o.val.p } {}
+
+	#if _BYTE_ORDER == _LITTLE_ENDIAN
+	inline _u64(u32 hi, u32 lo) : val{ .p = { .lo = lo, .hi = hi } } {}
+	#else
+	inline _u64(u32 hi, u32 lo) : val{ .p = { .hi = hi, .lo = lo } } {}
+	#endif
+};
+
+struct _u128
+{
+	typedef u64 utype;
+
+	union {
+		u8                          b[16];
+	#if _BYTE_ORDER == _LITTLE_ENDIAN
+		struct { u64 lo; u64 hi; }  p;
+	#else
+		struct { u64 hi; u64 lo; }  p;
+	#endif
+	} val;
+
+	inline _u128() : val{ .b = { 0 } } {}
+	inline _u128(const _u128 &o) : val{ .p = o.val.p } {}
+
+	#if _BYTE_ORDER == _LITTLE_ENDIAN
+	inline _u128(u64 hi, u64 lo) : val{ .p = { .lo = lo, .hi = hi } } {}
+	#else
+	inline _u128(u64 hi, u64 lo) : val{ .p = { .hi = hi, .lo = lo } } {}
+	#endif
+};
+
+template <typename R>
+R mult(typename R::utype x, typename R::utype y)
 {
 	/*
 	 *  Babbage multiplication, Passages from the Life of
@@ -30,6 +115,7 @@ R mult(T x, T y)
 	 *      +             |       x1 * y0       |
 	 *      +             |       x0 * y1       |
 	 *      +  |       x1 * y1       |
+	 *      +  |         C|         C|
 	 *
 	 *  z0  =  x0 * y0
 	 *  z1  =  x1 * y0
@@ -39,90 +125,69 @@ R mult(T x, T y)
 	 *  r   =  z0 + ((z1 + z2) << qb) + (z3 << hb)
 	 */
 
-	const int hb = sizeof(T) << 3;
+	typedef typename R::utype T;
 	const int qb = sizeof(T) << 2;
-	const T   mask = (1 << qb) - 1;
+	const T   mask = (T(1) << qb) - 1;
 
 	T x0 =    x       & mask;
 	T x1 =    x >> qb & mask;
-
 	T y0 =    y       & mask;
 	T y1 =    y >> qb & mask;
-
 	T z0 =    x0 * y0;
 	T z1 =    x1 * y0;
 	T z2 =    x0 * y1;
 	T z3 =    x1 * y1;
-
 	T z4 =    z1 + z2;
-	T carry = z4 < z1;
+	T c1 =    z4 < z1;
+	T lo =    z0 + (z4 << qb);
+	T c2 =    lo < z0;
+	T hi =    z3 + (z4 >> qb) + (c1 << qb) + c2;
 
-	// TODO - The final assembly needs to split the value into hi and lo
-	R r =     R(z0) + (R(z4) << qb) + (R(z3) << hb) + (R(carry) << (qb+hb));
-
-	if (debug) {
-		printf("\n");
-		printf("  x0        = 0x%016llx = %-20llu\n", (unsigned long long)x0, (unsigned long long)x0);
-		printf("  x1        = 0x%016llx = %-20llu\n", (unsigned long long)x1, (unsigned long long)x1);
-		printf("  y0        = 0x%016llx = %-20llu\n", (unsigned long long)y0, (unsigned long long)y0);
-		printf("  y1        = 0x%016llx = %-20llu\n", (unsigned long long)y1, (unsigned long long)y1);
-
-		R rc = R(x) * R(y);
-
-		printf("\n");
-		printf("  z0        = 0x%016llx = %-20llu\n", (unsigned long long)z0, (unsigned long long)z0);
-		printf("+ z1 << %-2d  = 0x%016llx = %-20llu\n", qb, (unsigned long long)(R(z1) << qb), (unsigned long long)(R(z1) << qb));
-		printf("+ z2 << %-2d  = 0x%016llx = %-20llu\n", qb, (unsigned long long)(R(z2) << qb), (unsigned long long)(R(z2) << qb));
-		printf("+ z3 << %-2d  = 0x%016llx = %-20llu\n", hb, (unsigned long long)(R(z3) << hb), (unsigned long long)(R(z3) << hb));
-
-		printf("\n");
-		printf("= r         = 0x%016llx = %-20llu\n", (unsigned long long)r, (unsigned long long)r);
-		printf("= rc        = 0x%016llx = %-20llu\n", (unsigned long long)rc, (unsigned long long)rc);
-		printf("= diff      = 0x%016llx = %-20llu\n\n", (unsigned long long)rc - r, (unsigned long long)rc - r);
-	}
-
-	return r;
+	return R(hi, lo);
 }
 
-template <typename R, typename T>
-void test_mult(T x, T y, const test_print_mode mode = test_print_all)
+template <typename L, typename R>
+void test_mult(typename R::utype x, typename R::utype y)
 {
-	R xy = mult<R,T>(x, y);
-	R xyc = R(x) * R(y);
-	bool pass = (xy == xyc);
-	if (mode == test_print_fail && pass) return;
-	printf("%s "
-		"%-11sx=%-40llu (0x%032llx)\n"
-		"%16sy=%-40llu (0x%032llx)\n"
-		"%15sxy=%-40llu (0x%032llx)\n"
-		"%14sxyc=%-40llu (0x%032llx)\n",
-		pass ? "PASS" : "FAIL", __func__,
-		(unsigned long long)x, (unsigned long long)x, "",
-		(unsigned long long)x, (unsigned long long)y, "",
-		(unsigned long long)xy, (unsigned long long)xy, "",
-		(unsigned long long)xyc, (unsigned long long)xyc);
-	if (mode == test_print_fail || mode == test_print_debug || !pass) {
-		mult<R,T,true>(x, y);
-	}
+	R xy = mult<R>(x, y);
+	L xya = (L(xy.val.p.hi) << (sizeof(L) << 2)) | L(xy.val.p.lo);
+	L xyc = L(x) * L(y);
+	bool pass = (xya == xyc);
+	char fmt[256];
+	const int hw = sizeof(L);
+	snprintf(fmt, sizeof(fmt), "%%s "
+		"%%-11sx=%%-20llu (0x%%0%ullx)\n"
+		"%%16sy=%%-20llu (0x%%0%ullx)\n"
+		"%%12sxy.hi=%%-20llu (0x%%0%ullx)\n"
+		"%%12sxy.lo=%%-20llu (0x%%0%ullx)\n"
+		"%%9sxy.check=%%-20llu (0x%%0%ullx)\n",
+		hw,hw,hw,hw,hw<<1);
+	printf(fmt, pass ? "PASS" : "FAIL", __func__,
+		(u64)x, (u64)x, "",
+		(u64)x, (u64)y, "",
+		(u64)xy.val.p.hi, (u64)xy.val.p.hi, "",
+		(u64)xy.val.p.lo, (u64)xy.val.p.lo, "",
+		(u64)xyc, (u64)xyc);
 }
 
 int main()
 {
-	test_mult<u16,u8>(53, 63);
-	test_mult<u16,u8>(53, 67);
-	test_mult<u16,u8>(63, 191);
-	test_mult<u16,u8>(253, 253);
-	test_mult<u32,u16>(12345, 999);
-	test_mult<u32,u16>(12345, 63000);
-	test_mult<u32,u16>(65533, 65533);
-	test_mult<u64,u32>(123450, 9990);
-	test_mult<u64,u32>(123450, 630000);
-	test_mult<u64,u32>(87652393, 87652393);
-	test_mult<u64,u32>(613566756, 613566756);
-	test_mult<u64,u32>(4294967295, 630000);
-	test_mult<u64,u32>(4294967295, 87652393);
-	test_mult<u64,u32>(4294967295, 613566756);
-	test_mult<u64,u32>(4294967295, 4294967295);
+	test_mult<u16,_u16>(53, 63);
+	test_mult<u16,_u16>(53, 67);
+	test_mult<u16,_u16>(63, 191);
+	test_mult<u16,_u16>(253, 253);
+	test_mult<u32,_u32>(12345, 999);
+	test_mult<u32,_u32>(12345, 63000);
+	test_mult<u32,_u32>(65533, 65533);
+	test_mult<u64,_u64>(123450, 9990);
+	test_mult<u64,_u64>(123450, 630000);
+	test_mult<u64,_u64>(87652393, 87652393);
+	test_mult<u64,_u64>(613566756, 613566756);
+	test_mult<u64,_u64>(4294967295, 630000);
+	test_mult<u64,_u64>(4294967295, 87652393);
+	test_mult<u64,_u64>(4294967295, 613566756);
+	test_mult<u64,_u64>(4294967295, 4294967295);
+	test_mult<unsigned __int128,_u128>(18446744073709551615ULL,18446744073709551615ULL);
 
 	return 0;
 }
