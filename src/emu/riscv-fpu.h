678d462c10650d92e92ec9dc5b7c0ddd6ee3aa74
Michael Clark <michael@metaparadigm.com>
2016-09-18 01:04:53 +1200

    Add helper functions for setting fscr flags and round mode

--- src/emu/riscv-fpu.h
+++ src/emu/riscv-fpu.h
@@ -7,6 +7,28 @@
 
 namespace riscv {
 
+	template <typename T>
+	inline void fenv_setflags(T &fcsr)
+	{
+		int flags = fetestexcept(FE_ALL_EXCEPT);
+		if (flags & FE_DIVBYZERO) fcsr |= riscv_fcsr_DZ;
+		if (flags & FE_INEXACT) fcsr |= riscv_fcsr_NX;
+		if (flags & FE_INVALID) fcsr |= riscv_fcsr_NV;
+		if (flags & FE_OVERFLOW) fcsr |= riscv_fcsr_OF;
+		if (flags & FE_UNDERFLOW) fcsr |= riscv_fcsr_UF;
+	}
+
+	inline void fenv_setrm(int rm)
+	{
+		switch (rm) {
+			case riscv_rm_rne: fesetround(FE_TONEAREST); /* ties to Even */ break;
+			case riscv_rm_rtz: fesetround(FE_TOWARDZERO); break;
+			case riscv_rm_rdn: fesetround(FE_DOWNWARD); break;
+			case riscv_rm_rup: fesetround(FE_UPWARD); break;
+			case riscv_rm_rmm: fesetround(FE_TONEAREST); /* ties to Max Magnitude */ break;
+		}
+	}
+
 	inline float f32_sqrt(float a) { return std::sqrt(a); }
 	inline double f64_sqrt(double a) { return std::sqrt(a); }
 
