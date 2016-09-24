5e9e0a60ba09956ed25b450117c20ddc57eee824
Michael Clark <michael@metaparadigm.com>
2016-09-24 22:03:57 +1200

    Fix typo in mstatus.hpie comment

--- src/emu/riscv-machine.h
+++ src/emu/riscv-machine.h
@@ -19,7 +19,7 @@ namespace riscv {
 			UX mie  : 1; /* 3     Machine mode Interrupt Enable */
 			UX upie : 1; /* 4     Prior User mode Interrupt Enable */
 			UX spie : 1; /* 5     Prior Supervisor mode Interrupt Enable */
-			UX hpie : 1; /* 6     Prior SHypervisor mode Interrupt Enable */
+			UX hpie : 1; /* 6     Prior Hypervisor mode Interrupt Enable */
 			UX mpie : 1; /* 7     Prior Machine mode Interrupt Enable */
 		/*	UX upp  : 0           URET pop privilege (implicitly 0) */
 			UX spp  : 1; /* 8     SRET pop privilege */
