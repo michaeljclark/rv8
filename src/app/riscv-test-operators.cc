3b8a8c2d05173204d8b60f2e19a98dad2841d1f9
Michael Clark <michael@metaparadigm.com>
2016-09-25 21:31:36 +1300

    Add constant time u128 and s128 operators: {<,>=,+,-,&,|,^,<<,>>}

--- src/app/riscv-test-operators.cc
+++ src/app/riscv-test-operators.cc
@@ -19,25 +19,131 @@ using namespace riscv;
 
 int main()
 {
+	// u128 operator==
 	assert(u128(0,0) == u128(0,0));
 	assert(!(u128(1,1) == u128(0,0)));
 	assert(u128(1,1) == u128(1,1));
 	assert(!(u128(1,1) == u128(2,2)));
 
+	// u128 operator!=
 	assert(!(u128(0,0) != u128(0,0)));
 	assert(u128(1,1) != u128(0,0));
 	assert(!(u128(1,1) != u128(1,1)));
 	assert(u128(1,1) != u128(2,2));
 
+	// u128 operator<
+	assert(!(u128(0,0) < u128(0,0)));
+	assert(u128(0,0) < u128(1,1));
+	assert(!(u128(0,1) < u128(0,0)));
+	assert(!(u128(1,0) < u128(0,0)));
+	assert(u128(1,0) < u128(1,1));
+	assert(!(u128(1,1) < u128(1,1)));
+	assert(!(u128(1,2) < u128(1,1)));
+
+	// u128 operator>=
+	assert(u128(0,0) >= u128(0,0));
+	assert(!(u128(0,0) >= u128(1,1)));
+	assert(u128(0,1) >= u128(0,0));
+	assert(u128(1,0) >= u128(0,0));
+	assert(!(u128(1,0) >= u128(1,1)));
+	assert(u128(1,1) >= u128(1,1));
+	assert(u128(1,2) >= u128(1,1));
+
+	// u128 operator+
+	assert(u128(1,1) + u128(2,2) == u128(3,3));
+	assert(u128(0,0xffffffffffffffffULL) + u128(0,0xffffffffffffffffULL) == u128(1,0xfffffffffffffffeULL));
+
+	// u128 operator-
+	assert(u128(3,3) - u128(2,2) == u128(1,1));
+	assert(u128(0,0xffffffffffffffffULL) - u128(0,0xffffffffffffffffULL) == u128(0,0));
+	assert(u128(1,0) - u128(0,0xffffffffffffffffULL) == u128(0,1));
+
+	// u128 operator&
+	assert((u128(0b010,0b101) & u128(0,0)) == u128(0,0));
+	assert((u128(0b010,0b101) & u128(0b110,0b100)) == u128(0b010,0b100));
+
+	// u128 operator|
+	assert((u128(0b010,0b010) | u128(0,0)) == u128(0b010,0b010));
+	assert((u128(0b010,0b010) | u128(0b010,0b010)) == u128(0b010,0b010));
+	assert((u128(0b010,0b010) | u128(0b110,0b101)) == u128(0b110,0b111));
+
+	// u128 operator^
+	assert((u128(0b010,0b010) ^ u128(0,0)) == u128(0b010,0b010));
+	assert((u128(0b010,0b010) ^ u128(0b010,0b010)) == u128(0,0));
+	assert((u128(0b010,0b010) ^ u128(0b110,0b101)) == u128(0b100,0b111));
+
+	// u128 operator<<
+	assert((u128(0,0xffffffffffffffffULL) << 1) == u128(1,0xfffffffffffffffeULL));
+	assert((u128(0,0x7fffffffffffffffULL) << 1) == u128(0,0xfffffffffffffffeULL));
+	assert((u128(0xf0,0xf0f0f0f0f0f0f0f0ULL) << 8) == u128(0xf0f0,0xf0f0f0f0f0f0f000ULL));
+	assert((u128(0xffffffffffffffffULL,0xffffffffffffffffULL) << 8) == u128(0xffffffffffffffffULL,0xffffffffffffff00ULL));
+
+	// u128 operator>>
+	assert((u128(0,0xffffffffffffffffULL) >> 1) == u128(0,0x7fffffffffffffffULL));
+	assert((u128(1,0xffffffffffffffffULL) >> 1) == u128(0,0xffffffffffffffffULL));
+	assert((u128(0xf0,0xf0f0f0f0f0f0f0f0ULL) >> 8) == u128(0,0xf0f0f0f0f0f0f0f0ULL));
+	assert((u128(0xffffffffffffffffULL,0xffffffffffffffffULL) >> 8) == u128(0x00ffffffffffffffULL,0xffffffffffffffffULL));
+
+	// s128 operator==
 	assert(s128(0,0) == s128(0,0));
 	assert(!(s128(1,1) == s128(0,0)));
 	assert(s128(-1,1) == s128(-1,1));
 	assert(!(s128(-1,1) == s128(-2,2)));
 
+	// s128 operator!=
 	assert(!(s128(0,0) != s128(0,0)));
 	assert(s128(1,1) != s128(0,0));
 	assert(!(s128(-1,1) != s128(-1,1)));
 	assert(s128(-1,1) != s128(-2,2));
+
+	// s128 operator<
+	assert(!(s128(-1,0) < s128(-1,0)));
+	assert(s128(0,0) < s128(1,1));
+	assert(!(s128(0,1) < s128(0,0)));
+	assert(!(s128(1,0) < s128(-1,0)));
+	assert(s128(1,0) < s128(1,1));
+	assert(!(s128(1,1) < s128(1,1)));
+	assert(!(s128(1,2) < s128(-1,1)));
+
+	// s128 operator>=
+	assert(s128(-1,0) >= s128(-1,0));
+	assert(!(s128(0,0) >= s128(1,1)));
+	assert(s128(0,1) >= s128(0,0));
+	assert(s128(1,0) >= s128(-1,0));
+	assert(!(s128(1,0) >= s128(1,1)));
+	assert(s128(1,1) >= s128(1,1));
+	assert(s128(1,2) >= s128(-1,1));
+
+	// s128 operator+
+	assert(s128(1,1) + s128(2,2) == s128(3,3));
+	assert(s128(0,0xffffffffffffffffULL) + s128(0,0xffffffffffffffffULL) == s128(1,0xfffffffffffffffeULL));
+
+	// s128 operator-
+	assert(s128(3,3) - s128(2,2) == s128(1,1));
+	assert(s128(0,0xffffffffffffffffULL) - s128(0,0xffffffffffffffffULL) == s128(0,0));
+	assert(s128(-1,0xffffffffffffffffULL) - s128(0,0xffffffffffffffffULL) == s128(-1,0));
+	assert(s128(1,0) - s128(0,0xffffffffffffffffULL) == s128(0,1));
+	assert(s128(0,0) - s128(0,0xffffffffffffffffULL) == s128(-1,1));
+
+	// s128 operator&
+	assert((s128(0b010,0b101) & s128(0,0)) == s128(0,0));
+	assert((s128(0b010,0b101) & s128(0b110,0b100)) == s128(0b010,0b100));
+
+	// s128 operator|
+	assert((s128(0b010,0b010) | s128(0,0)) == s128(0b010,0b010));
+	assert((s128(0b010,0b010) | s128(0b010,0b010)) == s128(0b010,0b010));
+	assert((s128(0b010,0b010) | s128(0b110,0b101)) == s128(0b110,0b111));
+
+	// s128 operator^
+	assert((s128(0b010,0b010) ^ s128(0,0)) == s128(0b010,0b010));
+	assert((s128(0b010,0b010) ^ s128(0b010,0b010)) == s128(0,0));
+	assert((s128(0b010,0b010) ^ s128(0b110,0b101)) == s128(0b100,0b111));
+
+	// s128 operator>>
+	assert((s128(0,0xffffffffffffffffULL) >> 1) == s128(0,0x7fffffffffffffffULL));
+	assert((s128(1,0xffffffffffffffffULL) >> 1) == s128(0,0xffffffffffffffffULL));
+	assert((s128(0xf0,0xf0f0f0f0f0f0f0f0ULL) >> 8) == s128(0,0xf0f0f0f0f0f0f0f0ULL));
+	assert((s128(0xffffffffffffffffULL,0xffffffffffffffffULL) >> 8) == s128(0xffffffffffffffffULL,0xffffffffffffffffULL));
 
 	return 0;
 }
--- src/asm/riscv-operators.h
+++ src/asm/riscv-operators.h
@@ -9,26 +9,131 @@
 namespace riscv
 {
 
-	/* bitwise operators cause the compiler to generate constant time code */
+	/* u128 - bitwise operators cause the compiler to generate constant time code */
 
-	inline bool operator==(u128 const&x, u128 const& y)
+	inline bool operator==(const u128 &x, const u128 &y)
 	{
 		return (x.r.d.lo == y.r.d.lo) & (x.r.d.hi == y.r.d.hi);
 	}
 
-	inline bool operator!=(u128 const&x, u128 const& y)
+	inline bool operator!=(const u128 &x, const u128 &y)
 	{
 		return (x.r.d.lo != y.r.d.lo) | (x.r.d.hi != y.r.d.hi);
 	}
 
-	inline bool operator==(s128 const&x, s128 const& y)
+	inline bool operator<(const u128 &x, const u128 &y)
+	{
+		return (x.r.d.hi < y.r.d.hi) | ((x.r.d.hi == y.r.d.hi) & (x.r.d.lo < y.r.d.lo));
+	}
+
+	inline bool operator>=(const u128 &x, const u128 &y)
+	{
+		return (x.r.d.hi > y.r.d.hi) | ((x.r.d.hi == y.r.d.hi) & (x.r.d.lo >= y.r.d.lo));
+	}
+
+	inline u128 operator&(const u128 &x, const u128 &y)
+	{
+		return u128(x.r.d.hi & y.r.d.hi, x.r.d.lo & y.r.d.lo);
+	}
+
+	inline u128 operator|(const u128 &x, const u128 &y)
+	{
+		return u128(x.r.d.hi | y.r.d.hi, x.r.d.lo | y.r.d.lo);
+	}
+
+	inline u128 operator^(const u128 &x, const u128 &y)
+	{
+		return u128(x.r.d.hi ^ y.r.d.hi, x.r.d.lo ^ y.r.d.lo);
+	}
+
+	inline u128 operator<<(const u128 &x, int shift)
+	{
+		u64 lo = x.r.d.lo << shift;
+		u64 hi = (x.r.d.hi << shift) | (x.r.d.lo >> (64 - shift));
+		return u128(hi, lo);
+	}
+
+	inline u128 operator>>(const u128 &x, int shift)
+	{
+		u64 lo = (x.r.d.lo >> shift) | (x.r.d.hi << (64 - shift));
+		u64 hi = x.r.d.hi >> shift;
+		return u128(hi, lo);
+	}
+
+	inline u128 operator+(const u128 &x, const u128 &y)
+	{
+		u64 lo = x.r.d.lo + y.r.d.lo;
+		u64 c = lo < x.r.d.lo;
+		u64 hi = x.r.d.hi + y.r.d.hi + c;
+		return u128(hi, lo);
+	}
+
+	inline u128 operator-(const u128 &x, const u128 &y)
+	{
+		u64 lo = x.r.d.lo - y.r.d.lo;
+		u64 b = lo > x.r.d.lo;
+		u64 hi = x.r.d.hi - y.r.d.hi - b;
+		return u128(hi, lo);
+	}
+
+	/* s128 - bitwise operators cause the compiler to generate constant time code */
+
+	inline bool operator==(const s128 &x, const s128 &y)
 	{
 		return (x.r.d.lo == y.r.d.lo) & (x.r.d.hi == y.r.d.hi);
 	}
 
-	inline bool operator!=(s128 const&x, s128 const& y)
+	inline bool operator!=(const s128 &x, const s128 &y)
 	{
 		return (x.r.d.lo != y.r.d.lo) | (x.r.d.hi != y.r.d.hi);
+	}
+
+	inline bool operator<(const s128 &x, const s128 &y)
+	{
+		return (x.r.d.hi < y.r.d.hi) | ((x.r.d.hi == y.r.d.hi) & (x.r.d.lo < y.r.d.lo));
+	}
+
+	inline bool operator>=(const s128 &x, const s128 &y)
+	{
+		return (x.r.d.hi > y.r.d.hi) | ((x.r.d.hi == y.r.d.hi) & (x.r.d.lo >= y.r.d.lo));
+	}
+
+	inline s128 operator&(const s128 &x, const s128 &y)
+	{
+		return s128(x.r.d.hi & y.r.d.hi, x.r.d.lo & y.r.d.lo);
+	}
+
+	inline s128 operator|(const s128 &x, const s128 &y)
+	{
+		return s128(x.r.d.hi | y.r.d.hi, x.r.d.lo | y.r.d.lo);
+	}
+
+	inline s128 operator^(const s128 &x, const s128 &y)
+	{
+		return s128(x.r.d.hi ^ y.r.d.hi, x.r.d.lo ^ y.r.d.lo);
+	}
+
+	inline s128 operator>>(const s128 &x, int shift)
+	{
+		u64 lo = (x.r.d.lo >> shift) | (x.r.d.hi << (64 - shift));
+		s64 hi = x.r.d.hi >> shift;
+		return s128(hi, lo);
+	}
+
+	inline s128 operator+(const s128 &x, const s128 &y)
+	{
+		u64 lo = x.r.d.lo + y.r.d.lo;
+		u64 c = lo < x.r.d.lo;
+		s64 hi = x.r.d.hi + y.r.d.hi + c;
+		return s128(hi, lo);
+	}
+
+	inline s128 operator-(const s128 &x, const s128 &y)
+	{
+		u64 lo = x.r.d.lo - y.r.d.lo;
+		u64 b = lo > x.r.d.lo;
+		s64 hi = x.r.d.hi - y.r.d.hi - b;
+		return s128(hi, lo);
 	}
 
 }
