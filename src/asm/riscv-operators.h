45527092d321e3bea6543df703f0e4946448e31e
Michael Clark <michael@metaparadigm.com>
2016-10-02 21:32:24 +1300

    Add 128 bit operators (*,/,-,~). Fix shift bugs and add tests.
    - Fix 128-bit >>,<< bugs
    - Add constant time predicate logic comment to >>,<<
    - Add 128-bit signed and unsigned multiply *
    - Add 128-bit signed division /
    - Add 128-bit unary negate -
    - Add 128-bit bitwise not ~

--- src/asm/riscv-operators.h
+++ src/asm/riscv-operators.h
@@ -56,6 +56,11 @@ namespace riscv
 		return u128(x.r.d.hi ^ y.r.d.hi, x.r.d.lo ^ y.r.d.lo);
 	}
 
+	inline u128 operator~(const u128 &x)
+	{
+		return u128(~x.r.d.hi, ~x.r.d.lo);
+	}
+
 	inline u128 operator&(const u128 &x, s32 y)
 	{
 		return u128(x.r.d.hi, x.r.d.lo & y);
@@ -73,15 +78,17 @@ namespace riscv
 
 	inline u128 operator<<(const u128 &x, int shift)
 	{
-		u64 lo = x.r.d.lo << shift;
-		u64 hi = (x.r.d.hi << shift) | (x.r.d.lo >> (64 - shift));
+		// todo - use predicate logic instead of ternary operator - violates constant time property
+		u64 hi = (shift == 0) ? x.r.d.hi : (shift < 64) ? (x.r.d.hi << shift) | (x.r.d.lo >> (64 - shift)) : x.r.d.lo << (shift - 64);
+		u64 lo = (shift == 0) ? x.r.d.lo : (shift < 64) ? (x.r.d.lo << shift) : 0;
 		return u128(hi, lo);
 	}
 
 	inline u128 operator>>(const u128 &x, int shift)
 	{
-		u64 lo = (x.r.d.lo >> shift) | (x.r.d.hi << (64 - shift));
-		u64 hi = x.r.d.hi >> shift;
+		// todo - use predicate logic instead of ternary operator - violates constant time property
+		u64 hi = (shift == 0) ? x.r.d.hi : (shift < 64) ? (x.r.d.hi >> shift) : 0;
+		u64 lo = (shift == 0) ? x.r.d.lo : (shift < 64) ? (x.r.d.lo >> shift) | (x.r.d.hi << (64 - shift)) : x.r.d.hi >> (shift - 64);
 		return u128(hi, lo);
 	}
 
@@ -99,6 +106,49 @@ namespace riscv
 		u64 b = lo > x.r.d.lo;
 		u64 hi = x.r.d.hi - y.r.d.hi - b;
 		return u128(hi, lo);
+	}
+
+	inline u128 operator-(const u128 &x)
+	{
+		return ~x + 1;
+	}
+
+	inline u128 operator*(const u128 &x, const u128 &y)
+	{
+		u128 z0 = x.r.d.lo * y.r.d.lo;
+		u128 z1 = x.r.d.hi * y.r.d.lo;
+		u128 z2 = x.r.d.lo * y.r.d.hi;
+		u128 z4 = z1 + z2;
+		u128 c1 = z4 < z1;
+		return ((z4 + c1) << 64) + z0;
+	}
+
+	inline u128 operator/(const u128 &x, const u128 &y)
+	{
+		if (y == 0) return u128(-1,-1);
+		u128 q = 0, r = 0;
+		for (int i = 127; i >= 0; i--) {
+			r = r << 1;
+			r = r | ((x >> i) & u128(1));
+			u128 d = r >= y;
+			r = r - (y & -d);
+			q = q | (d << i);
+		}
+		return q;
+	}
+
+	inline u128 operator%(const u128 &x, const u128 &y)
+	{
+		if (y == 0) return x;
+		u128 q = 0, r = 0;
+		for (int i = 127; i >= 0; i--) {
+			r = r << 1;
+			r = r | ((x >> i) & u128(1));
+			u128 d = r >= y;
+			r = r - (y & -d);
+			q = q | (d << i);
+		}
+		return r;
 	}
 
 	/* s128 - bitwise operators cause the compiler to generate constant time code */
@@ -148,6 +198,11 @@ namespace riscv
 		return s128(x.r.d.hi ^ y.r.d.hi, x.r.d.lo ^ y.r.d.lo);
 	}
 
+	inline s128 operator~(const s128 &x)
+	{
+		return s128(~x.r.d.hi, ~x.r.d.lo);
+	}
+
 	inline s128 operator&(const s128 &x, s32 y)
 	{
 		return s128(x.r.d.hi, x.r.d.lo & y);
@@ -163,10 +218,19 @@ namespace riscv
 		return s128(x.r.d.hi, x.r.d.lo ^ y);
 	}
 
+	inline s128 operator<<(const s128 &x, int shift)
+	{
+		// todo - use predicate logic instead of ternary operator - violates constant time property
+		u64 hi = (shift == 0) ? x.r.d.hi : (shift < 64) ? (x.r.d.hi << shift) | (x.r.d.lo >> (64 - shift)) : x.r.d.lo << (shift - 64);
+		u64 lo = (shift == 0) ? x.r.d.lo : (shift < 64) ? (x.r.d.lo << shift) : 0;
+		return s128(hi, lo);
+	}
+
 	inline s128 operator>>(const s128 &x, int shift)
 	{
-		u64 lo = (x.r.d.lo >> shift) | (x.r.d.hi << (64 - shift));
-		s64 hi = x.r.d.hi >> shift;
+		// todo - use predicate logic instead of ternary operator - violates constant time property
+		s64 hi = (shift == 0) ? x.r.d.hi : (shift < 64) ? (x.r.d.hi >> shift) : (x.r.d.hi >> 63) >> 1; // moron
+		u64 lo = (shift == 0) ? x.r.d.lo : (shift < 64) ? (x.r.d.lo >> shift) | (x.r.d.hi << (64 - shift)) : x.r.d.hi >> (shift - 64);
 		return s128(hi, lo);
 	}
 
@@ -184,6 +248,21 @@ namespace riscv
 		u64 b = lo > x.r.d.lo;
 		s64 hi = x.r.d.hi - y.r.d.hi - b;
 		return s128(hi, lo);
+	}
+
+	inline s128 operator-(const s128 &x)
+	{
+		return ~x + 1;
+	}
+
+	inline s128 operator*(const s128 &x, const s128 &y)
+	{
+		s128 z0 = x.r.d.lo * y.r.d.lo;
+		s128 z1 = x.r.d.hi * y.r.d.lo;
+		s128 z2 = x.r.d.lo * y.r.d.hi;
+		s128 z4 = z1 + z2;
+		s128 c1 = z4 < z1;
+		return ((z4 + c1) << 64) + z0;
 	}
 
 }
