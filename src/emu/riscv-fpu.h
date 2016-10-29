820cfa1d77cbe64cfb77f57b4db6c22ad155a254
Michael Clark <michael@metaparadigm.com>
2016-10-29 22:14:29 +1300

    Change {f32,f64}_classify to use union and meta type template
    - Add f32_bits and f64_bits floating point bitfield unions
    - Add f32_type and f64_type floating point metadata types
    - Add {f32,f64}_{man_size,exp_size,exp_denorm,exp_inf} constants
    - Change {f32,f64}_classify to a meta type template implementation

--- src/emu/riscv-fpu.h
+++ src/emu/riscv-fpu.h
@@ -7,6 +7,70 @@
 
 namespace riscv {
 
+	/* IEEE-754 Single-precision floating point */
+
+	union f32_bits {
+		f32 f;
+		struct {
+		#if _BYTE_ORDER == _LITTLE_ENDIAN
+			u32 man  : 23;
+			u32 exp  : 8;
+			u32 sign : 1;
+		#else
+			u32 sign : 1;
+			u32 exp  : 8;
+			u32 man  : 23;
+		#endif
+		} r;
+	};
+
+	struct f32_type {
+		typedef f32      float_type;
+		typedef u32      size_type;
+		typedef f32_bits bits_type;
+		enum {
+			man_size = 23,
+			man_digits = man_size + 1,
+			exp_size = 8,
+			exp_max = 128,
+			exp_denorm = 0,
+			exp_inf = (1 << exp_size) - 1,
+			dec_digits = 9
+		};
+	};
+
+	/* IEEE-754 Double-precision floating point */
+
+	union f64_bits {
+		f64 f;
+		struct {
+		#if _BYTE_ORDER == _LITTLE_ENDIAN
+			u64 man  : 52;
+			u64 exp  : 11;
+			u64 sign : 1;
+		#else
+			u64 sign : 1;
+			u64 exp  : 11;
+			u64 man  : 52;
+		#endif
+		} r;
+	};
+
+	struct f64_type {
+		typedef f64      float_type;
+		typedef u64      size_type;
+		typedef f64_bits bits_type;
+		enum {
+			man_size = 52,
+			man_digits = man_size + 1,
+			exp_size = 11,
+			exp_max = 1024,
+			exp_denorm = 0,
+			exp_inf = (1 << exp_size) - 1,
+			dec_digits = 17
+		};
+	};
+
 	/* get accrued exception flags (C11) */
 
 	template <typename T>
@@ -54,8 +118,8 @@ namespace riscv {
 	inline s32 fcvt_w(T &fcsr, F f)
 	{
 		return (((f > std::numeric_limits<s32>::max()) & (s32(f) < 0)) | std::isnan(f))
-				? std::numeric_limits<s32>::max()
-				: s32(f);
+			? std::numeric_limits<s32>::max()
+			: s32(f);
 	}
 
 	/* convert single or double to unsigned word (32-bit) */
@@ -64,12 +128,12 @@ namespace riscv {
 	inline s32 fcvt_wu(T &fcsr, F f)
 	{
 		return (std::isnan(f) | ((f >= 0) & std::isinf(f)))
-				? std::numeric_limits<u32>::max()
-				: f >= 0
-				? s32(u32(f))
-				: f > -1
-				? (fcsr |= riscv_fcsr_NX, s32(0))
-				: (fcsr |= riscv_fcsr_NV, s32(0));
+			? std::numeric_limits<u32>::max()
+			: f >= 0
+			? s32(u32(f))
+			: f > -1
+			? (fcsr |= riscv_fcsr_NX, s32(0))
+			: (fcsr |= riscv_fcsr_NV, s32(0));
 	}
 
 	/* convert single or double to signed long (64-bit) */
@@ -78,8 +142,8 @@ namespace riscv {
 	inline s64 fcvt_l(T &fcsr, F f)
 	{
 		return (((f > std::numeric_limits<s64>::max()) & (s64(f) < 0)) | std::isnan(f))
-				? std::numeric_limits<s64>::max()
-				: s64(f);
+			? std::numeric_limits<s64>::max()
+			: s64(f);
 	}
 
 	/* convert single or double to unsigned long (64-bit) */
@@ -88,59 +152,39 @@ namespace riscv {
 	inline s64 fcvt_lu(T &fcsr, F f)
 	{
 		return (std::isnan(f) | ((f >= 0) & std::isinf(f)))
-				? std::numeric_limits<u64>::max()
-				: f >= 0
-				? s64(u64(f))
-				: f > -1
-				? (fcsr |= riscv_fcsr_NX, s64(0))
-				: (fcsr |= riscv_fcsr_NV, s64(0));
+			? std::numeric_limits<u64>::max()
+			: f >= 0
+			? s64(u64(f))
+			: f > -1
+			? (fcsr |= riscv_fcsr_NX, s64(0))
+			: (fcsr |= riscv_fcsr_NV, s64(0));
 	}
 
-	/* single precision square root */
+	/* floating point square root */
 
 	inline f32 f32_sqrt(f32 a) { return std::sqrt(a); }
-
-	/* double precision square root */
-
 	inline f64 f64_sqrt(f64 a) { return std::sqrt(a); }
 
-	/* single precision classify */
+	/* floating point classify */
 
-	inline int f32_classify(float a)
+	template <typename F>
+	inline int float_classify(typename F::float_type f)
 	{
-		union { uint32_t wu; float f; } v = { .f = a };
-		uint32_t neg = v.wu >> 31, exp = (v.wu >> 23) & ((1<<8)-1), man = v.wu & ((1<<23)-1);
-		bool inf = exp == ((1<<8)-1);
-		bool denorm = exp == 0;
-		if (denorm) {
-			if (man == 0) return neg ? riscv_fclass_neg_zero : riscv_fclass_pos_zero;
-			else return neg ? riscv_fclass_neg_subnorm : riscv_fclass_pos_subnorm;
-		} else if (inf) {
-			if (man == 0) return neg ? riscv_fclass_neg_inf : riscv_fclass_pos_inf;
-			else return man & 1 ? riscv_fclass_signaling_nan : riscv_fclass_quiet_nan;
-		} else {
-			return neg ? riscv_fclass_neg_norm : riscv_fclass_pos_norm;
-		}
+		typename F::bits_type v{ .f = f };
+		typename F::size_type neg = v.r.sign, exp = v.r.exp, man = v.r.man;
+		return (exp == F::exp_denorm)
+			? (man == 0)
+			? (neg ? riscv_fclass_neg_zero : riscv_fclass_pos_zero)
+			: (neg ? riscv_fclass_neg_subnorm : riscv_fclass_pos_subnorm)
+			: (exp == F::exp_inf)
+			? (man == 0)
+			? (neg ? riscv_fclass_neg_inf : riscv_fclass_pos_inf)
+			: (man & 1 ? riscv_fclass_signaling_nan : riscv_fclass_quiet_nan)
+			: (neg ? riscv_fclass_neg_norm : riscv_fclass_pos_norm);
 	}
 
-	/* double precision classify */
-
-	inline int f64_classify(double a)
-	{
-		union { uint64_t lu; double d; } v = { .d = a };
-		uint64_t neg = v.lu >> 63, exp = (v.lu >> 52) & ((1ULL<<11)-1), man = v.lu & ((1ULL<<52)-1);
-		bool inf = exp == ((1ULL<<11)-1);
-		bool denorm = exp == 0;
-		if (denorm) {
-			if (man == 0) return neg ? riscv_fclass_neg_zero : riscv_fclass_pos_zero;
-			else return neg ? riscv_fclass_neg_subnorm : riscv_fclass_pos_subnorm;
-		} else if (inf) {
-			if (man == 0) return neg ? riscv_fclass_neg_inf : riscv_fclass_pos_inf;
-			else return man & 1 ? riscv_fclass_signaling_nan : riscv_fclass_quiet_nan;
-		} else {
-			return neg ? riscv_fclass_neg_norm : riscv_fclass_pos_norm;
-		}
-	}
+	inline int f32_classify(f32 f) { return float_classify<f32_type>(f); }
+	inline int f64_classify(f64 f) { return float_classify<f64_type>(f); }
 
 }
 
