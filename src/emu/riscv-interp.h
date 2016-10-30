5d89c01ab8ac7857122a199d13fd686e12971ddf
Michael Clark <michael@metaparadigm.com>
2016-10-30 17:53:41 +1300

    Clear lowest bit on indirect JALR calls
    - Ignore the low bit of the calculated offset for indirect JALR
      calls. This assumes the program counter is already aligned. e.g.
    
        pc_offset = (rs1 + imm - pc) & ~1

--- src/emu/riscv-interp.h
+++ src/emu/riscv-interp.h
@@ -35,7 +35,7 @@ riscv::addr_t exec_inst_rv32(T &dec, P &proc, riscv::addr_t pc_offset)
 			break;
 		case riscv_op_jalr:
 			if (rvi) {
-				if (dec.rd > 0) proc.ireg[dec.rd] = proc.pc + pc_offset; pc_offset = proc.ireg[dec.rs1] + dec.imm - proc.pc;
+				if (dec.rd > 0) proc.ireg[dec.rd] = proc.pc + pc_offset; pc_offset = (proc.ireg[dec.rs1] + dec.imm - proc.pc) & ~1;
 			};
 			break;
 		case riscv_op_beq:
@@ -591,7 +591,7 @@ riscv::addr_t exec_inst_rv64(T &dec, P &proc, riscv::addr_t pc_offset)
 			break;
 		case riscv_op_jalr:
 			if (rvi) {
-				if (dec.rd > 0) proc.ireg[dec.rd] = proc.pc + pc_offset; pc_offset = proc.ireg[dec.rs1] + dec.imm - proc.pc;
+				if (dec.rd > 0) proc.ireg[dec.rd] = proc.pc + pc_offset; pc_offset = (proc.ireg[dec.rs1] + dec.imm - proc.pc) & ~1;
 			};
 			break;
 		case riscv_op_beq:
