cf0b017072b792454a308f0c5fc39334062b5b5f
Michael Clark <michael@metaparadigm.com>
2016-11-04 21:50:55 +1300

    Move arg_list into util/riscv-printf-types.h

--- src/util/riscv-printf-types.h
+++ src/util/riscv-printf-types.h
@@ -31,6 +31,69 @@ namespace riscv {
 		double             dbl;
 	};
 
+	/* argument list */
+
+	struct arg_list
+	{
+		const arg_type *at;
+		const type_holder *th;
+		const int elem;
+		int nextarg;
+
+		inline arg_list(const arg_type *at, const type_holder *th, const int elem) :
+			at(at), th(th), elem(elem), nextarg(0) {}
+
+		inline bool check(arg_type type) {
+			if (nextarg >= elem) return false;
+			if (type != arg_type_none && at[nextarg] != type) return false;
+			return true;
+		}
+
+		inline bool get_int(unsigned long long &val) {
+			if (check(arg_type_none)) {
+				val = th[nextarg++].ull;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		inline bool get_dbl(double &val) {
+			if (check(arg_type_dbl)) {
+				val = th[nextarg++].dbl;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		inline bool get_char(int &val) {
+			if (check(arg_type_char)) {
+				val = th[nextarg++].sll;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		inline bool get_str(std::string &str) {
+			if (!check(arg_type_none)) {
+				return false;
+			} else if (th[nextarg].ptr == nullptr) {
+				str = "(null)";
+				nextarg++;
+				return true;
+			} else if (check(arg_type_sstr)) {
+				str = *th[nextarg++].sstr;
+				return true;
+			} else if (check(arg_type_cstr)) {
+				str = th[nextarg++].cstr;
+				return true;
+			} else {
+				return false;
+			}
+		}
+	};
 }
 
 #endif
