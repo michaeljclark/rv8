16361db72e4add729db44b5fd7c26e99c7ef3e40
Michael Clark <michael@metaparadigm.com>
2016-11-04 21:48:28 +1300

    Whitespace fix

--- src/util/riscv-printf-io.h
+++ src/util/riscv-printf-io.h
@@ -115,39 +115,39 @@ namespace riscv
 		io_pad(buf, len - str_len, with);
 	}
 
-	static int
-	exponent(char *p0, int expo, int fmtch)
+	static int exponent(char *p0, int expo, int fmtch)
 	{
 		char *p, *t;
 		char expbuf[MAXEXPDIG];
 		
 		p = p0;
-		*p++ = fmtch; 
+		*p++ = fmtch;
 		if (expo < 0) {
-				expo = -expo;
-				*p++ = '-';
+			expo = -expo;
+			*p++ = '-';
 		}
-		else    
-				*p++ = '+';
+		else {
+			*p++ = '+';
+		}
 		t = expbuf + MAXEXPDIG;
 		if (expo > 9) {
-				do {    
-						*--t = to_char(expo % 10);
-				} while ((expo /= 10) > 9);
-				*--t = to_char(expo);
-				for (; t < expbuf + MAXEXPDIG; *p++ = *t++)
-						;
+			do {
+				*--t = to_char(expo % 10);
+			} while ((expo /= 10) > 9);
+			*--t = to_char(expo);
+			for (; t < expbuf + MAXEXPDIG; *p++ = *t++);
 		}
-		else {  
-				/* 
-				 * Exponents for decimal floating point conversions
-				 * (%[eEgG]) must be at least two characters long,
-				 * whereas exponents for hexadecimal conversions can
-				 * be only one character long.
-				 */
-				if (fmtch == 'e' || fmtch == 'E')
-						*p++ = '0';
-				*p++ = to_char(expo);
+		else {
+			/*
+			 * Exponents for decimal floating point conversions
+			 * (%[eEgG]) must be at least two characters long,
+			 * whereas exponents for hexadecimal conversions can
+			 * be only one character long.
+			 */
+			if (fmtch == 'e' || fmtch == 'E') {
+				*p++ = '0';
+			}
+			*p++ = to_char(expo);
 		}
 		return (p - p0);
 	}
