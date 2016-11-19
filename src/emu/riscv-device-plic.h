b577225a29fed987c25518b1c0430eb1dd324498
Michael Clark <michael@metaparadigm.com>
2016-11-20 10:39:39 +1300

    Implement PLIC device state
    NOTE: MMIO load and store interface needs to be implemented

--- src/emu/riscv-device-plic.h
+++ src/emu/riscv-device-plic.h
@@ -9,18 +9,39 @@ namespace riscv {
 
 	/* PLIC MMIO device */
 
-	template <typename P>
+	template <typename P, const int NUM_IRQS = 64>
 	struct plic_mmio_device : memory_segment<typename P::ux>
 	{
 		typedef typename P::ux UX;
 
+		enum {
+			num_irqs = NUM_IRQS,
+			priority_bits = 2,
+			bits_per_word = sizeof(UX) << 3,
+			word_shift = ctz_pow2(bits_per_word),
+			irq_words = num_irqs / bits_per_word,
+			priority_words = (num_irqs / bits_per_word) * priority_bits,
+			pending_size = sizeof(UX) * irq_words,
+			enabled_size = sizeof(UX) * irq_words,
+			priority_size = sizeof(UX) * priority_words,
+			total_size = pending_size + enabled_size + priority_size
+		};
+
 		P &proc;
 
-		/* TODO - implement PLIC state */
+		UX pending[irq_words];
+		UX enabled[irq_words];
+		UX priority[priority_words];
 
 		plic_mmio_device(P &proc, UX mpa) :
-			memory_segment<UX>(mpa, /*uva*/0, /*size*/0x1000,
+			memory_segment<UX>(mpa, /*uva*/0, /*size*/total_size,
 				pma_type_io | pma_prot_read | pma_prot_write), proc(proc) {}
+
+		void signal_irq(UX irq)
+		{
+			irq &= (num_irqs-1);
+			pending[irq >> word_shift] |= (1ULL << (irq & (bits_per_word-1)));
+		}
 
 		void load_8 (UX va, u8  &val) { printf("plic_mmio:0x%04llx -> 0\n", addr_t(va)); val = 0; }
 		void load_16(UX va, u16 &val) { printf("plic_mmio:0x%04llx -> 0\n", addr_t(va)); val = 0; }
