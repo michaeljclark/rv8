2b98f0d23735f70ad5f99937707f3d278285dfac
Michael Clark <michael@metaparadigm.com>
2016-11-20 12:51:57 +1300

    Implement UART device state
    NOTE: MMIO interface needs to be implemented

--- src/emu/riscv-device-uart.h
+++ src/emu/riscv-device-uart.h
@@ -16,10 +16,25 @@ namespace riscv {
 
 		P &proc;
 
-		/* TODO - implement UART state */
+		struct {
+			u8 RBR; /* (R  [0]) Recieve Buffer Register        */
+			u8 THR; /* (W  [0]) Transmit Holding Register      */
+			u8 IER; /* (RW [1]) Interrupt Enable Register      */
+			u8 IIR; /* (R  [2]) Interrupt Identity Register    */
+			u8 FCR; /* (W  [2]) FIFO Control Register          */
+			u8 LCR; /* (RW [3]) Line Control Register          */
+			u8 MCR; /* (RW [4]) MODEM Control Register         */
+			u8 LSR; /* (RW [5]) Line Status Register           */
+			u8 MSR; /* (RW [6]) MODEM Status Register          */
+			u8 SCR; /* (RW [7]) Scratch Register               */
+			u8 DLL; /* (RW [0]) Divisor Latch LSB (LCR.DLAB=1) */
+			u8 DLM; /* (RW [0]) Divisor Latch MSB (LCR.DLAB=1) */
+		} regs;
+
+		/* Register MMIO offsets in brackets (Based on 16550)  */
 
 		uart_mmio_device(P &proc, UX mpa) :
-			memory_segment<UX>(mpa, /*uva*/0, /*size*/0x1000,
+			memory_segment<UX>(mpa, /*uva*/0, /*size*/8,
 				pma_type_io | pma_prot_read | pma_prot_write), proc(proc) {}
 
 		void load_8 (UX va, u8  &val) { printf("uart_mmio:0x%04llx -> 0\n", addr_t(va)); val = 0; }
