e6930b410fb0bfa9739170c28a90cdba8a283706
Michael Clark <michael@metaparadigm.com>
2016-11-20 10:06:14 +1300

    Change PLIC, UART and TIME MMIO device virtual address offsets to 0

--- src/emu/riscv-device-plic.h
+++ src/emu/riscv-device-plic.h
@@ -19,19 +19,18 @@ namespace riscv {
 		/* TODO - implement PLIC state */
 
 		plic_mmio_device(P &proc, UX mpa) :
-			memory_segment<UX>(mpa, 0x1000, 0x1000, pma_type_io | pma_prot_read | pma_prot_write), proc(proc) {}
+			memory_segment<UX>(mpa, /*uva*/0, /*size*/0x1000,
+				pma_type_io | pma_prot_read | pma_prot_write), proc(proc) {}
 
-		~plic_mmio_device() {}
+		void load_8 (UX va, u8  &val) { printf("plic_mmio:0x%04llx -> 0\n", addr_t(va)); val = 0; }
+		void load_16(UX va, u16 &val) { printf("plic_mmio:0x%04llx -> 0\n", addr_t(va)); val = 0; }
+		void load_32(UX va, u32 &val) { printf("plic_mmio:0x%04llx -> 0\n", addr_t(va)); val = 0; }
+		void load_64(UX va, u64 &val) { printf("plic_mmio:0x%04llx -> 0\n", addr_t(va)); val = 0; }
 
-		void load_8 (UX va, u8  &val) { printf("plic_mmio:0x%llx -> 0\n", addr_t(va)); val = 0; }
-		void load_16(UX va, u16 &val) { printf("plic_mmio:0x%llx -> 0\n", addr_t(va)); val = 0; }
-		void load_32(UX va, u32 &val) { printf("plic_mmio:0x%llx -> 0\n", addr_t(va)); val = 0; }
-		void load_64(UX va, u64 &val) { printf("plic_mmio:0x%llx -> 0\n", addr_t(va)); val = 0; }
-
-		void store_8 (UX va, u8  val) { printf("plic_mmio:0x%llx <- 0x%02hhx\n", addr_t(va), val); }
-		void store_16(UX va, u16 val) { printf("plic_mmio:0x%llx <- 0x%04hx\n", addr_t(va), val); }
-		void store_32(UX va, u32 val) { printf("plic_mmio:0x%llx <- 0x%08x\n", addr_t(va), val); }
-		void store_64(UX va, u64 val) { printf("plic_mmio:0x%llx <- 0x%016llx\n", addr_t(va), val); }
+		void store_8 (UX va, u8  val) { printf("plic_mmio:0x%04llx <- 0x%02hhx\n", addr_t(va), val); }
+		void store_16(UX va, u16 val) { printf("plic_mmio:0x%04llx <- 0x%04hx\n", addr_t(va), val); }
+		void store_32(UX va, u32 val) { printf("plic_mmio:0x%04llx <- 0x%04x\n", addr_t(va), val); }
+		void store_64(UX va, u64 val) { printf("plic_mmio:0x%04llx <- 0x%016llx\n", addr_t(va), val); }
 	};
 
 }
