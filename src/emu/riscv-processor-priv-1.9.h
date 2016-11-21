1d4067616a57c94b79d6bd8dbe2d73e2828a129c
Michael Clark <michael@metaparadigm.com>
2016-11-21 16:30:54 +1300

    Handle interrupts in priority order. Return after frst trap is taken.

--- src/emu/riscv-processor-priv-1.9.h
+++ src/emu/riscv-processor-priv-1.9.h
@@ -496,29 +496,6 @@ namespace riscv {
 
 		void isr()
 		{
-			/* service timer interrupts if enabled */
-
-			bool timer_pending = device_time->timer_pending(P::time);
-			if (P::mstatus.r.mie && timer_pending) {
-				if (P::mideleg & (1 << riscv_intr_m_timer)) {
-					if (P::hideleg & (1 << riscv_intr_h_timer)) {
-						if (P::sideleg & (1 << riscv_intr_s_timer) && P::mie.r.utie) {
-							P::mip.r.utip = 1;
-							utrap(riscv_intr_u_timer, true);
-						} else if (P::mie.r.stie) {
-							P::mip.r.stip = 1;
-							strap(riscv_intr_s_timer, true);
-						}
-					} else if (P::mie.r.htie) {
-						P::mip.r.htip = 1;
-						htrap(riscv_intr_h_timer, true);
-					}
-				} else if (P::mie.r.mtie) {
-					P::mip.r.mtip = 1;
-					mtrap(riscv_intr_m_timer, true);
-				}
-			}
-
 			/* service external interrupts */
 
 			bool irq_pending = device_plic->irq_pending(P::mode, P::node_id, P::hart_id);
@@ -528,17 +505,48 @@ namespace riscv {
 						if (P::sideleg & (1 << riscv_intr_s_external) && P::mie.r.ueie) {
 							P::mip.r.ueip = 1;
 							utrap(riscv_intr_u_external, true);
+							return;
 						} else if (P::mie.r.seie) {
 							P::mip.r.seip = 1;
 							strap(riscv_intr_s_external, true);
+							return;
 						}
 					} else if (P::mie.r.heie) {
 						P::mip.r.heip = 1;
 						htrap(riscv_intr_h_external, true);
+						return;
 					}
 				} else if (P::mie.r.meie) {
 					P::mip.r.meip = 1;
 					mtrap(riscv_intr_m_external, true);
+					return;
+				}
+			}
+
+			/* service timer interrupts if enabled */
+
+			bool timer_pending = device_time->timer_pending(P::time);
+			if (P::mstatus.r.mie && timer_pending) {
+				if (P::mideleg & (1 << riscv_intr_m_timer)) {
+					if (P::hideleg & (1 << riscv_intr_h_timer)) {
+						if (P::sideleg & (1 << riscv_intr_s_timer) && P::mie.r.utie) {
+							P::mip.r.utip = 1;
+							utrap(riscv_intr_u_timer, true);
+							return;
+						} else if (P::mie.r.stie) {
+							P::mip.r.stip = 1;
+							strap(riscv_intr_s_timer, true);
+							return;
+						}
+					} else if (P::mie.r.htie) {
+						P::mip.r.htip = 1;
+						htrap(riscv_intr_h_timer, true);
+						return;
+					}
+				} else if (P::mie.r.mtie) {
+					P::mip.r.mtip = 1;
+					mtrap(riscv_intr_m_timer, true);
+					return;
 				}
 			}
 
@@ -551,17 +559,21 @@ namespace riscv {
 						if (P::sideleg & (1 << riscv_intr_s_software) && P::mie.r.ueie) {
 							P::mip.r.usip = 1;
 							utrap(riscv_intr_u_software, true);
+							return;
 						} else if (P::mie.r.ssie) {
 							P::mip.r.ssip = 1;
 							strap(riscv_intr_s_software, true);
+							return;
 						}
 					} else if (P::mie.r.hsie) {
 						P::mip.r.hsip = 1;
 						htrap(riscv_intr_h_software, true);
+						return;
 					}
 				} else if (P::mie.r.msie) {
 					P::mip.r.msip = 1;
 					mtrap(riscv_intr_m_software, true);
+					return;
 				}
 			}
 
