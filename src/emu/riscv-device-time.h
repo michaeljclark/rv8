3cd10e92d8541ed2ed97a24f67022469718dc54e
Michael Clark <michael@metaparadigm.com>
2016-12-03 18:51:09 +1300

    Make timer one shot and implicity claim when signalling pending

--- src/emu/riscv-device-time.h
+++ src/emu/riscv-device-time.h
@@ -24,13 +24,18 @@ namespace riscv {
 
 		u64 mtime;
 		u64 mtimecmp;
+		bool claimed;
 
 		/* Timer constructor */
 
 		time_mmio_device(P &proc, UX mpa) :
 			memory_segment<UX>("TIME", mpa, /*uva*/0, /*size*/total_size,
 				pma_type_io | pma_prot_read | pma_prot_write),
-			proc(proc) {}
+			proc(proc),
+			mtime(0),
+			mtimecmp(0),
+			claimed(true)
+		{}
 
 		/* Timer interface */
 
@@ -43,7 +48,12 @@ namespace riscv {
 		bool timer_pending(UX time)
 		{
 			mtime = time;
-			return mtime >= mtimecmp;
+			if (!claimed) {
+				claimed = mtime >= mtimecmp;
+				return claimed;
+			} else {
+				return false;
+			}
 		}
 
 		/* Timer MMIO */
@@ -66,10 +76,18 @@ namespace riscv {
 
 		void load_32(UX va, u32 &val)
 		{
-			if (va == 0) val = u32(mtime);
-			else if (va == 4) val = u32(mtime >> 32);
-			else if (va == 8) val = u32(mtimecmp);
-			else if (va == 12) val = u32(mtimecmp >> 32);
+			if (va == 0) {
+				val = u32(mtime);
+			}
+			else if (va == 4) {
+				val = u32(mtime >> 32);
+			}
+			else if (va == 8) {
+				val = u32(mtimecmp);
+			}
+			else if (va == 12) {
+				val = u32(mtimecmp >> 32);
+			}
 			if (proc.log & proc_log_mmio) {
 				printf("time_mmio:0x%04llx -> 0x%04x\n", addr_t(va), val);
 			}
@@ -77,8 +95,12 @@ namespace riscv {
 
 		void load_64(UX va, u64 &val)
 		{
-			if (va == 0) val = mtime;
-			else if (va == 8) val = mtimecmp;
+			if (va == 0) {
+				val = mtime;
+			}
+			else if (va == 8) {
+				val = mtimecmp;
+			}
 			if (proc.log & proc_log_mmio) {
 				printf("time_mmio:0x%04llx -> 0x%08llx\n", addr_t(va), val);
 			}
@@ -103,10 +125,20 @@ namespace riscv {
 			if (proc.log & proc_log_mmio) {
 				printf("time_mmio:0x%04llx <- 0x%04x\n", addr_t(va), val);
 			}
-			if (va == 0) mtime = (mtime & (u64(-1) << 32)) | val;
-			else if (va == 4) mtime = (mtime & (u64(-1) >> 32)) | (u64(val) << 32);
-			else if (va == 8) mtimecmp = (mtimecmp & (u64(-1) << 32)) | val;
-			else if (va == 12) mtimecmp = (mtimecmp & (u64(-1) >> 32)) | (u64(val) << 32);
+			if (va == 0) {
+				mtime = (mtime & (u64(-1) << 32)) | val;
+			}
+			else if (va == 4) {
+				mtime = (mtime & (u64(-1) >> 32)) | (u64(val) << 32);
+			}
+			else if (va == 8) {
+				claimed = false;
+				mtimecmp = (mtimecmp & (u64(-1) << 32)) | val;
+			}
+			else if (va == 12) {
+				claimed = false;
+				mtimecmp = (mtimecmp & (u64(-1) >> 32)) | (u64(val) << 32);
+			}
 		}
 
 		void store_64(UX va, u64 val)
@@ -114,8 +146,13 @@ namespace riscv {
 			if (proc.log & proc_log_mmio) {
 				printf("time_mmio:0x%04llx <- 0x%08llx\n", addr_t(va), val);
 			}
-			if (va == 0) mtime = val;
-			else if (va == 8) mtimecmp = val;
+			if (va == 0) {
+				mtime = val;
+			}
+			else if (va == 8) {
+				claimed = false;
+				mtimecmp = val;
+			}
 		}
 	};
 
