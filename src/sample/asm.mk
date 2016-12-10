ee02d1b7f39f49401f7a44da70dad71668437c7f
Michael Clark <michael@metaparadigm.com>
2016-12-10 20:56:23 +1300

    Stick to loops for initial asm comparision
    - Update Makefile macros
    - Remove test-sieve and test-sort
    - Add -fno-vectorize to disable clang 4.0 vectorization

--- src/sample/asm.mk
+++ src/sample/asm.mk
@@ -5,12 +5,21 @@
 # Requires RISCV environment variable to be set
 # RISCV = /opt/riscv/toolchain
 
-CC = ${RISCV}/bin/${TARGET}-gcc -march=$(ARCH)
-CXX = ${RISCV}/bin/${TARGET}-g++ -march=$(ARCH)
-AS = ${RISCV}/bin/${TARGET}-as
-LD = ${RISCV}/bin/${TARGET}-ld
-STRIP = ${RISCV}/bin/${TARGET}-strip
-PK = ${RISCV}/${TARGET}/bin/pk
+HOST_CC = cc
+HOST_CXX = c++
+
+TARGET_CC = ${RISCV}/bin/${TARGET}-gcc -march=$(ARCH)
+TARGET_CXX = ${RISCV}/bin/${TARGET}-g++ -march=$(ARCH)
+
+SRC_DIR = src/sample
+ASM_DIR = build/$(TARGET_DIR)/sample-asm
+BIN_DIR = build/$(TARGET_DIR)/sample-bin
+
+HOST_CFLAGS = -O1 -Wall -fPIC -fno-vectorize
+HOST_CXXFLAGS = -std=c++1y -fno-exceptions -fno-rtti $(HOST_CFLAGS)
+
+TARGET_CFLAGS = -O1 -Wall -fPIC
+TARGET_CXXFLAGS = -std=c++1y -fno-exceptions -fno-rtti $(TARGET_CFLAGS)
 
 ifeq ($(RVC),1)
 CC += -mrvc
@@ -19,13 +28,6 @@ TARGET_DIR = $(TARGET)-rvc
 else
 TARGET_DIR = $(TARGET)
 endif
-
-SRC_DIR = src/sample
-ASM_DIR = build/$(TARGET_DIR)/sample-asm
-BIN_DIR = build/$(TARGET_DIR)/sample-bin
-
-CFLAGS = -O1 -Wall -fPIC 
-CXXFLAGS = -std=c++1y -fno-exceptions -fno-rtti $(CFLAGS)
 
 ASSEMBLY = \
 	$(ASM_DIR)/test-loop-1-rv64.s \
@@ -37,11 +39,7 @@ ASSEMBLY = \
 	$(ASM_DIR)/test-loop-4-rv64.s \
 	$(ASM_DIR)/test-loop-4-amd64.s \
 	$(ASM_DIR)/test-loop-5-rv64.s \
-	$(ASM_DIR)/test-loop-5-amd64.s \
-	$(ASM_DIR)/test-sieve-rv64.s \
-	$(ASM_DIR)/test-sieve-amd64.s \
-	$(ASM_DIR)/test-sort-rv64.s \
-	$(ASM_DIR)/test-sort-amd64.s
+	$(ASM_DIR)/test-loop-5-amd64.s
 
 all: dirs $(ASSEMBLY)
 
@@ -50,23 +48,17 @@ dirs: ; @mkdir -p $(ASM_DIR)
 clean:
 	rm -f $(PROGRAMS) *.o
 
-$(ASM_DIR)/test-loop-1-rv64.s: $(SRC_DIR)/test-loop-1.cc ; $(CXX) $(CXXFLAGS) -S $^ -o $@
-$(ASM_DIR)/test-loop-1-amd64.s: $(SRC_DIR)/test-loop-1.cc ; c++ $(CXXFLAGS) -S $^ -o $@
+$(ASM_DIR)/test-loop-1-rv64.s: $(SRC_DIR)/test-loop-1.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
+$(ASM_DIR)/test-loop-1-amd64.s: $(SRC_DIR)/test-loop-1.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
 
-$(ASM_DIR)/test-loop-2-rv64.s: $(SRC_DIR)/test-loop-2.cc ; $(CXX) $(CXXFLAGS) -S $^ -o $@
-$(ASM_DIR)/test-loop-2-amd64.s: $(SRC_DIR)/test-loop-2.cc ; c++ $(CXXFLAGS) -S $^ -o $@
+$(ASM_DIR)/test-loop-2-rv64.s: $(SRC_DIR)/test-loop-2.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
+$(ASM_DIR)/test-loop-2-amd64.s: $(SRC_DIR)/test-loop-2.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
 
-$(ASM_DIR)/test-loop-3-rv64.s: $(SRC_DIR)/test-loop-3.cc ; $(CXX) $(CXXFLAGS) -S $^ -o $@
-$(ASM_DIR)/test-loop-3-amd64.s: $(SRC_DIR)/test-loop-3.cc ; c++ $(CXXFLAGS) -S $^ -o $@
+$(ASM_DIR)/test-loop-3-rv64.s: $(SRC_DIR)/test-loop-3.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
+$(ASM_DIR)/test-loop-3-amd64.s: $(SRC_DIR)/test-loop-3.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
 
-$(ASM_DIR)/test-loop-4-rv64.s: $(SRC_DIR)/test-loop-4.cc ; $(CXX) $(CXXFLAGS) -S $^ -o $@
-$(ASM_DIR)/test-loop-4-amd64.s: $(SRC_DIR)/test-loop-4.cc ; c++ $(CXXFLAGS) -S $^ -o $@
+$(ASM_DIR)/test-loop-4-rv64.s: $(SRC_DIR)/test-loop-4.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
+$(ASM_DIR)/test-loop-4-amd64.s: $(SRC_DIR)/test-loop-4.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
 
-$(ASM_DIR)/test-loop-5-rv64.s: $(SRC_DIR)/test-loop-5.cc ; $(CXX) $(CXXFLAGS) -S $^ -o $@
-$(ASM_DIR)/test-loop-5-amd64.s: $(SRC_DIR)/test-loop-5.cc ; c++ $(CXXFLAGS) -S $^ -o $@
-
-$(ASM_DIR)/test-sieve-rv64.s: $(SRC_DIR)/test-sieve.cc ; $(CXX) $(CXXFLAGS) -S $^ -o $@
-$(ASM_DIR)/test-sieve-amd64.s: $(SRC_DIR)/test-sieve.cc ; c++ $(CXXFLAGS) -S $^ -o $@
-
-$(ASM_DIR)/test-sort-rv64.s: $(SRC_DIR)/test-sort.cc ; $(CC) $(CXXFLAGS) -S $^ -o $@
-$(ASM_DIR)/test-sort-amd64.s: $(SRC_DIR)/test-sort.cc ; c++ $(CXXFLAGS) -S $^ -o $@
+$(ASM_DIR)/test-loop-5-rv64.s: $(SRC_DIR)/test-loop-5.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
+$(ASM_DIR)/test-loop-5-amd64.s: $(SRC_DIR)/test-loop-5.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
