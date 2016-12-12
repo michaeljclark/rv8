ceeda74a47fc9bb60b77ee4395b93fea3b03d967
Michael Clark <michael@metaparadigm.com>
2016-12-12 21:33:39 +1300

    Move LaTeX type diagram into a function
    - Hoist type diagram generator into a function
    - Make option match generator and command line flag

--- src/gen/riscv-gen-latex-alt.cc
+++ src/gen/riscv-gen-latex-alt.cc
@@ -21,9 +21,9 @@
 std::vector<cmdline_option> riscv_gen_latex_alt::get_cmdline_options()
 {
 	return std::vector<cmdline_option>{
-		{ "-la", "--print-alt-latex", cmdline_arg_type_none,
+		{ "-la", "--print-latex-alt", cmdline_arg_type_none,
 			"Print alternative LaTeX instruction listing",
-			[&](std::string s) { return gen->set_option("print_alt"); } },
+			[&](std::string s) { return gen->set_option("print_latex_alt"); } },
 	};
 }
 
@@ -89,10 +89,97 @@ struct riscv_latex_page
 	std::vector<riscv_latex_row> rows;
 };
 
-static void print_alt(riscv_gen *gen)
+static void print_latex_alt_type(riscv_gen *gen, riscv_type_ptr type)
 {
 	typedef std::vector<riscv_bitspec> riscv_bitspec_list;
 
+	printf("%s", kLatexPictureBegin);
+
+	// find bit ranges for each named decode component
+	std::map<std::string,riscv_bitspec_list> decode_list;
+	std::vector<std::string> decode_names;
+
+	for (auto &part : type->parts) {
+		riscv_bitspec &bitspec = part.first;
+		std::string &name = part.second;
+		auto bi = decode_list.find(name);
+		if (bi == decode_list.end()) {
+			bi = decode_list.insert(decode_list.end(),
+				std::pair<std::string,riscv_bitspec_list>(name, riscv_bitspec_list()));
+			decode_names.push_back(name);
+		}
+		bi->second.push_back(bitspec);
+	}
+
+	// assign shades
+	std::map<std::string,int> decode_shade;
+	int shade = (decode_list.size() -1) * 10;
+	for (auto &name : decode_names) {
+		decode_shade[name] = shade;
+		shade -= 10;
+	}
+
+	// draw bit segment fill
+	for (auto &part : type->parts) {
+		riscv_bitspec &bitspec = part.first;
+		std::string &name = part.second;
+		int msb = bitspec.segments.front().first.msb;
+		int lsb = bitspec.segments.back().first.lsb;
+		int shade = decode_shade[name];
+		if (shade == 0) continue;
+		printf("\\fill [black!%d] (%d,-1)  rectangle (%d,-2);\n", shade, 32 - lsb, (32 - msb) - 1);
+	}
+	printf("\n");
+
+	// draw bit segment borders
+	for (auto &part : type->parts) {
+		riscv_bitspec &bitspec = part.first;
+		int msb = bitspec.segments.front().first.msb;
+		int lsb = bitspec.segments.back().first.lsb;
+		printf("\\draw (%d,-1) rectangle (%d,-2);\n", 32 - lsb, (32 - msb) - 1);
+	}
+	printf("\n");
+
+	// draw bit segment bit position labels
+	for (auto &part : type->parts) {
+		riscv_bitspec &bitspec = part.first;
+		int msb = bitspec.segments.front().first.msb;
+		int lsb = bitspec.segments.back().first.lsb;
+		if (msb == lsb) {
+			printf("\\node (inst_%d)    at (%f ,-1.5) {$%d$};\n", msb, (32 - msb) - 0.5, msb);
+		} else {
+			printf("\\node (inst_%d)    at (%f ,-1.5) {$%d$};\n", msb, (32 - msb) - 0.5, msb);
+			printf("\\node (inst_%d)    at (%f ,-1.5) {$%d$};\n", lsb, (32 - lsb) - 0.5, lsb);
+		}
+	}
+	printf("\n");
+
+	// draw bit segment text labels
+	for (auto &part : type->parts) {
+		riscv_bitspec &bitspec = part.first;
+		std::string &name = part.second;
+
+		std::string label = name;
+		size_t decode_bits = 0;
+		for (auto &segment : bitspec.segments) {
+			decode_bits += segment.second.size();
+		}
+		if (decode_bits > 0) {
+			label = label + bitspec.to_string();
+		}
+
+		int msb = bitspec.segments.front().first.msb;
+		int lsb = bitspec.segments.back().first.lsb;
+		float center = lsb + (msb - lsb) / 2;
+		printf("\\node (%s) at (%f,-0.5) {$%s$};\n", name.c_str(), (32 - center) - 0.5, label.c_str());
+	}
+	printf("\n");
+
+	printf("%s", kLatexPictureEnd);
+}
+
+static void print_latex_alt(riscv_gen *gen)
+{
 	// print document header
 	printf("%s", kLatexDocumentBegin);
 
@@ -105,92 +192,10 @@ static void print_alt(riscv_gen *gen)
 		std::string type_name = type->name;
 		std::transform(type_name.begin(), type_name.end(), type_name.begin(), ::toupper);
 		type_name = "Type-" + type_name;
-
 		printf("\n\\bf{%s}\n", type_name.c_str());
 
-		printf("%s", kLatexPictureBegin);
-
-		// find bit ranges for each named decode component
-		std::map<std::string,riscv_bitspec_list> decode_list;
-		std::vector<std::string> decode_names;
-
-		for (auto &part : type->parts) {
-			riscv_bitspec &bitspec = part.first;
-			std::string &name = part.second;
-			auto bi = decode_list.find(name);
-			if (bi == decode_list.end()) {
-				bi = decode_list.insert(decode_list.end(),
-					std::pair<std::string,riscv_bitspec_list>(name, riscv_bitspec_list()));
-				decode_names.push_back(name);
-			}
-			bi->second.push_back(bitspec);
-		}
-
-		// assign shades
-		std::map<std::string,int> decode_shade;
-		int shade = (decode_list.size() -1) * 10;
-		for (auto &name : decode_names) {
-			decode_shade[name] = shade;
-			shade -= 10;
-		}
-
-		// draw bit segment fill
-		for (auto &part : type->parts) {
-			riscv_bitspec &bitspec = part.first;
-			std::string &name = part.second;
-			int msb = bitspec.segments.front().first.msb;
-			int lsb = bitspec.segments.back().first.lsb;
-			int shade = decode_shade[name];
-			if (shade == 0) continue;
-			printf("\\fill [black!%d] (%d,-1)  rectangle (%d,-2);\n", shade, 32 - lsb, (32 - msb) - 1);
-		}
-		printf("\n");
-
-		// draw bit segment borders
-		for (auto &part : type->parts) {
-			riscv_bitspec &bitspec = part.first;
-			int msb = bitspec.segments.front().first.msb;
-			int lsb = bitspec.segments.back().first.lsb;
-			printf("\\draw (%d,-1) rectangle (%d,-2);\n", 32 - lsb, (32 - msb) - 1);
-		}
-		printf("\n");
-
-		// draw bit segment bit position labels
-		for (auto &part : type->parts) {
-			riscv_bitspec &bitspec = part.first;
-			int msb = bitspec.segments.front().first.msb;
-			int lsb = bitspec.segments.back().first.lsb;
-			if (msb == lsb) {
-				printf("\\node (inst_%d)    at (%f ,-1.5) {$%d$};\n", msb, (32 - msb) - 0.5, msb);
-			} else {
-				printf("\\node (inst_%d)    at (%f ,-1.5) {$%d$};\n", msb, (32 - msb) - 0.5, msb);
-				printf("\\node (inst_%d)    at (%f ,-1.5) {$%d$};\n", lsb, (32 - lsb) - 0.5, lsb);
-			}
-		}
-		printf("\n");
-
-		// draw bit segment text labels
-		for (auto &part : type->parts) {
-			riscv_bitspec &bitspec = part.first;
-			std::string &name = part.second;
-
-			std::string label = name;
-			size_t decode_bits = 0;
-			for (auto &segment : bitspec.segments) {
-				decode_bits += segment.second.size();
-			}
-			if (decode_bits > 0) {
-				label = label + bitspec.to_string();
-			}
-
-			int msb = bitspec.segments.front().first.msb;
-			int lsb = bitspec.segments.back().first.lsb;
-			float center = lsb + (msb - lsb) / 2;
-			printf("\\node (%s) at (%f,-0.5) {$%s$};\n", name.c_str(), (32 - center) - 0.5, label.c_str());
-		}
-		printf("\n");
-
-		printf("%s", kLatexPictureEnd);
+		// output the type diagram
+		print_latex_alt_type(gen, type);
 	}
 
 	// print document trailer
@@ -199,5 +204,5 @@ static void print_alt(riscv_gen *gen)
 
 void riscv_gen_latex_alt::generate()
 {
-	if (gen->has_option("print_alt")) print_alt(gen);
+	if (gen->has_option("print_latex_alt")) print_latex_alt(gen);
 }
