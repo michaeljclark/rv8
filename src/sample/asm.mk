614113eb727eb56d4a72eb3f6c86b6b5d75c3cee
Michael Clark <michael@metaparadigm.com>
2016-12-16 06:17:51 +1300

    Rename intermodule samples as 1a, 1b instead of 1 and 2, ...

--- src/sample/asm.mk
+++ src/sample/asm.mk
@@ -41,14 +41,14 @@ ASSEMBLY = \
 	$(ASM_DIR)/test-loop-4-amd64.s \
 	$(ASM_DIR)/test-loop-5-rv64.s \
 	$(ASM_DIR)/test-loop-5-amd64.s \
-	$(ASM_DIR)/test-module-1-rv64.s \
-	$(ASM_DIR)/test-module-1-amd64.s \
-	$(ASM_DIR)/test-module-2-rv64.s \
-	$(ASM_DIR)/test-module-2-amd64.s \
-	$(ASM_DIR)/test-module-3-rv64.s \
-	$(ASM_DIR)/test-module-3-amd64.s \
-	$(ASM_DIR)/test-module-4-rv64.s \
-	$(ASM_DIR)/test-module-4-amd64.s \
+	$(ASM_DIR)/test-module-1a-rv64.s \
+	$(ASM_DIR)/test-module-1a-amd64.s \
+	$(ASM_DIR)/test-module-1b-rv64.s \
+	$(ASM_DIR)/test-module-1b-amd64.s \
+	$(ASM_DIR)/test-module-2a-rv64.s \
+	$(ASM_DIR)/test-module-2a-amd64.s \
+	$(ASM_DIR)/test-module-2b-rv64.s \
+	$(ASM_DIR)/test-module-2b-amd64.s \
 	$(ASM_DIR)/test-thread-1-rv64.s \
 	$(ASM_DIR)/test-thread-1-amd64.s
 
@@ -63,14 +63,14 @@ OBJECTS = \
 	$(OBJ_DIR)/test-loop-4-amd64.o \
 	$(OBJ_DIR)/test-loop-5-rv64.o \
 	$(OBJ_DIR)/test-loop-5-amd64.o \
-	$(OBJ_DIR)/test-module-1-rv64.o \
-	$(OBJ_DIR)/test-module-1-amd64.o \
-	$(OBJ_DIR)/test-module-2-rv64.o \
-	$(OBJ_DIR)/test-module-2-amd64.o \
-	$(OBJ_DIR)/test-module-3-rv64.o \
-	$(OBJ_DIR)/test-module-3-amd64.o \
-	$(OBJ_DIR)/test-module-4-rv64.o \
-	$(OBJ_DIR)/test-module-4-amd64.o \
+	$(OBJ_DIR)/test-module-1a-rv64.o \
+	$(OBJ_DIR)/test-module-1a-amd64.o \
+	$(OBJ_DIR)/test-module-1b-rv64.o \
+	$(OBJ_DIR)/test-module-1b-amd64.o \
+	$(OBJ_DIR)/test-module-2a-rv64.o \
+	$(OBJ_DIR)/test-module-2a-amd64.o \
+	$(OBJ_DIR)/test-module-2b-rv64.o \
+	$(OBJ_DIR)/test-module-2b-amd64.o \
 	$(OBJ_DIR)/test-thread-1-rv64.o \
 	$(OBJ_DIR)/test-thread-1-amd64.o
 
@@ -106,25 +106,25 @@ $(OBJ_DIR)/test-loop-5-rv64.o: $(SRC_DIR)/test-loop-5.cc ; $(TARGET_CXX) $(TARGE
 $(ASM_DIR)/test-loop-5-amd64.s: $(SRC_DIR)/test-loop-5.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
 $(OBJ_DIR)/test-loop-5-amd64.o: $(SRC_DIR)/test-loop-5.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -c $^ -o $@
 
-$(ASM_DIR)/test-module-1-rv64.s: $(SRC_DIR)/test-module-1.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
-$(OBJ_DIR)/test-module-1-rv64.o: $(SRC_DIR)/test-module-1.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -c $^ -o $@
-$(ASM_DIR)/test-module-1-amd64.s: $(SRC_DIR)/test-module-1.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
-$(OBJ_DIR)/test-module-1-amd64.o: $(SRC_DIR)/test-module-1.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -c $^ -o $@
+$(ASM_DIR)/test-module-1a-rv64.s: $(SRC_DIR)/test-module-1a.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
+$(OBJ_DIR)/test-module-1a-rv64.o: $(SRC_DIR)/test-module-1a.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -c $^ -o $@
+$(ASM_DIR)/test-module-1a-amd64.s: $(SRC_DIR)/test-module-1a.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
+$(OBJ_DIR)/test-module-1a-amd64.o: $(SRC_DIR)/test-module-1a.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -c $^ -o $@
 
-$(ASM_DIR)/test-module-2-rv64.s: $(SRC_DIR)/test-module-2.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
-$(OBJ_DIR)/test-module-2-rv64.o: $(SRC_DIR)/test-module-2.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -c $^ -o $@
-$(ASM_DIR)/test-module-2-amd64.s: $(SRC_DIR)/test-module-2.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
-$(OBJ_DIR)/test-module-2-amd64.o: $(SRC_DIR)/test-module-2.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -c $^ -o $@
+$(ASM_DIR)/test-module-1b-rv64.s: $(SRC_DIR)/test-module-1b.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
+$(OBJ_DIR)/test-module-1b-rv64.o: $(SRC_DIR)/test-module-1b.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -c $^ -o $@
+$(ASM_DIR)/test-module-1b-amd64.s: $(SRC_DIR)/test-module-1b.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
+$(OBJ_DIR)/test-module-1b-amd64.o: $(SRC_DIR)/test-module-1b.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -c $^ -o $@
 
-$(ASM_DIR)/test-module-3-rv64.s: $(SRC_DIR)/test-module-3.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
-$(OBJ_DIR)/test-module-3-rv64.o: $(SRC_DIR)/test-module-3.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -c $^ -o $@
-$(ASM_DIR)/test-module-3-amd64.s: $(SRC_DIR)/test-module-3.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
-$(OBJ_DIR)/test-module-3-amd64.o: $(SRC_DIR)/test-module-3.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -c $^ -o $@
+$(ASM_DIR)/test-module-2a-rv64.s: $(SRC_DIR)/test-module-2a.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
+$(OBJ_DIR)/test-module-2a-rv64.o: $(SRC_DIR)/test-module-2a.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -c $^ -o $@
+$(ASM_DIR)/test-module-2a-amd64.s: $(SRC_DIR)/test-module-2a.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
+$(OBJ_DIR)/test-module-2a-amd64.o: $(SRC_DIR)/test-module-2a.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -c $^ -o $@
 
-$(ASM_DIR)/test-module-4-rv64.s: $(SRC_DIR)/test-module-4.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
-$(OBJ_DIR)/test-module-4-rv64.o: $(SRC_DIR)/test-module-4.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -c $^ -o $@
-$(ASM_DIR)/test-module-4-amd64.s: $(SRC_DIR)/test-module-4.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
-$(OBJ_DIR)/test-module-4-amd64.o: $(SRC_DIR)/test-module-4.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -c $^ -o $@
+$(ASM_DIR)/test-module-2b-rv64.s: $(SRC_DIR)/test-module-2b.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
+$(OBJ_DIR)/test-module-2b-rv64.o: $(SRC_DIR)/test-module-2b.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -c $^ -o $@
+$(ASM_DIR)/test-module-2b-amd64.s: $(SRC_DIR)/test-module-2b.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -S $^ -o $@
+$(OBJ_DIR)/test-module-2b-amd64.o: $(SRC_DIR)/test-module-2b.cc ; $(HOST_CXX) $(HOST_CXXFLAGS) -c $^ -o $@
 
 $(ASM_DIR)/test-thread-1-rv64.s: $(SRC_DIR)/test-thread-1.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -S $^ -o $@
 $(OBJ_DIR)/test-thread-1-rv64.o: $(SRC_DIR)/test-thread-1.cc ; $(TARGET_CXX) $(TARGET_CXXFLAGS) -c $^ -o $@
