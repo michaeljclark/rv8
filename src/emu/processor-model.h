ff0fb837c8962908bf04e7ece742f42bc102b8d8
Michael Clark <michael@metaparadigm.com>
2017-01-02 05:04:58 +1300

    Add RVI processor model

--- src/emu/processor-model.h
+++ src/emu/processor-model.h
@@ -12,16 +12,32 @@ namespace riscv {
 	#define RV_32  /*rv32*/true,  /*rv64*/false
 	#define RV_64  /*rv32*/false, /*rv64*/true
 
-	#define RV_IMA    /*I*/true, /*M*/true, /*A*/true, /*S*/true, /*F*/false,/*D*/false,/*C*/false
-	#define RV_IMAC   /*I*/true, /*M*/true, /*A*/true, /*S*/true, /*F*/false,/*D*/false,/*C*/true
-	#define RV_IMAFD  /*I*/true, /*M*/true, /*A*/true, /*S*/true, /*F*/true, /*D*/true, /*C*/false
-	#define RV_IMAFDC /*I*/true, /*M*/true, /*A*/true, /*S*/true, /*F*/true, /*D*/true, /*C*/true
+	#define RV_I      /*I*/true, /*M*/false, /*A*/false, /*S*/true, /*F*/false,/*D*/false,/*C*/false
+	#define RV_IMA    /*I*/true, /*M*/true,  /*A*/true,  /*S*/true, /*F*/false,/*D*/false,/*C*/false
+	#define RV_IMAC   /*I*/true, /*M*/true,  /*A*/true,  /*S*/true, /*F*/false,/*D*/false,/*C*/true
+	#define RV_IMAFD  /*I*/true, /*M*/true,  /*A*/true,  /*S*/true, /*F*/true, /*D*/true, /*C*/false
+	#define RV_IMAFDC /*I*/true, /*M*/true,  /*A*/true,  /*S*/true, /*F*/true, /*D*/true, /*C*/true
 
 	template <typename P>
 	static constexpr typename P::ux BASE(rv_isa isa) { return typename P::ux(isa) << (P::xlen-2); }
 	static constexpr int EXT(char x) { return 1 << (x - 'A'); }
 
-	/* RV32 Partial processor specialization templates (RV32IMA, RV32IMAC, RV32IMAFD, RV32IMAFDC) */
+	/* RV32 Partial processor specialization templates (RV32I, RV32IMA, RV32IMAC, RV32IMAFD, RV32IMAFDC) */
+
+	template <typename T, typename P, typename M, typename B = processor_impl<T,P,M>>
+	struct processor_rv32i_model : B
+	{
+		const typename P::ux misa_default = BASE<P>(rv_isa_rv32)
+			| EXT('I');
+
+		void inst_decode(T &dec, inst_t inst) {
+			decode_inst<T,RV_32,RV_I>(dec, inst);
+		}
+
+		addr_t inst_exec(T &dec, addr_t pc_offset) {
+			return exec_inst_rv32<RV_I>(dec, *this, pc_offset);
+		}
+	};
 
 	template <typename T, typename P, typename M, typename B = processor_impl<T,P,M>>
 	struct processor_rv32ima_model : B
@@ -86,7 +102,22 @@ namespace riscv {
 	};
 
 
-	/* RV64 Partial processor specialization templates (RV64IMA, RV64IMAC, RV64IMAFD, RV64IMAFDC) */
+	/* RV64 Partial processor specialization templates (RV64I, RV64IMA, RV64IMAC, RV64IMAFD, RV64IMAFDC) */
+
+	template <typename T, typename P, typename M, typename B = processor_impl<T,P,M>>
+	struct processor_rv64i_model : B
+	{
+		const typename P::ux misa_default = BASE<P>(rv_isa_rv64)
+			| EXT('I');
+
+		void inst_decode(T &dec, inst_t inst) {
+			decode_inst<T,RV_64,RV_I>(dec, inst);
+		}
+
+		addr_t inst_exec(T &dec, addr_t pc_offset) {
+			return exec_inst_rv64<RV_I>(dec, *this, pc_offset);
+		}
+	};
 
 	template <typename T, typename P, typename M, typename B = processor_impl<T,P,M>>
 	struct processor_rv64ima_model : B
