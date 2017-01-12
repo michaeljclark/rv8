c7d5452a487444c676d91c3123141285350e97c3
Michael Clark <michael@metaparadigm.com>
2017-01-13 12:42:05 +1300

    Remove adhoc -mrvc parameters from compiler arguments

--- src/test/test.mk
+++ src/test/test.mk
@@ -174,8 +174,8 @@ $(BIN_DIR)/test-m-mret-user: $(OBJ_DIR)/test-m-mret-user.o ; $(LD) $^ -o $@
 $(OBJ_DIR)/test-m-sv39.o: $(SRC_DIR)/test-m-sv39.S ; $(CC) -c $^ -o $@
 $(BIN_DIR)/test-m-sv39: $(OBJ_DIR)/test-m-sv39.o ; $(LD) $^ -o $@
 
-$(OBJ_DIR)/test-sbi-info.o: $(SRC_DIR)/test-sbi-info.c ; $(CC) -fPIC -mrvc -Os -c $^ -o $@
+$(OBJ_DIR)/test-sbi-info.o: $(SRC_DIR)/test-sbi-info.c ; $(CC) -fPIC -Os -c $^ -o $@
 $(BIN_DIR)/test-sbi-info: $(OBJ_DIR)/test-sbi-info.o ; $(CC) -Wl,--no-relax -nostartfiles $^ -o $@
 
-$(OBJ_DIR)/test-sbi-timer.o: $(SRC_DIR)/test-sbi-timer.c ; $(CC) -fPIC -mrvc -Os -c $^ -o $@
+$(OBJ_DIR)/test-sbi-timer.o: $(SRC_DIR)/test-sbi-timer.c ; $(CC) -fPIC -Os -c $^ -o $@
 $(BIN_DIR)/test-sbi-timer: $(OBJ_DIR)/test-sbi-timer.o ; $(CC) -Wl,--no-relax -nostartfiles $^ -o $@
