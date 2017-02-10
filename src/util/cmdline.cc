05ab2fb83c4257fba8ee080ae5ea7ced234701da
Michael Clark <michael@metaparadigm.com>
2017-02-10 15:00:43 +1300

    Allow command line options to be interleaved with non option arguments

--- src/util/cmdline.cc
+++ src/util/cmdline.cc
@@ -24,6 +24,7 @@ std::pair<arg_list,bool> cmdline_option::process_options(cmdline_option *options
 		args.push_back(argv[i]);
 	}
 	
+	arg_list remaining_args;
 	while (args.size() > 0) {
 		std::string arg = args.front();
 		cmdline_option *o = options, *found_opt = nullptr;
@@ -34,28 +35,25 @@ std::pair<arg_list,bool> cmdline_option::process_options(cmdline_option *options
 			}
 			o++;
 		}
-		if (!found_opt) {
-			if (arg.size() > 0 && arg[0] == '-') {
-				fprintf(stderr, "%s: invalid option: %s\n", argv[0], arg.c_str());
-				return std::pair<arg_list,bool>(args, false);
-			}
-			break;
-		}
 		args.pop_front();
+		if (!found_opt) {
+			remaining_args.push_back(arg);
+			continue;
+		}
 		std::string s;
 		if (found_opt->arg_type != cmdline_arg_type_none) {
 			if (args.size() == 0) {
-				return std::pair<arg_list,bool>(args, false);
+				return std::pair<arg_list,bool>(arg_list(), false);
 			}
 			s = args.front();
 			args.pop_front();
 		}
 		if (!found_opt->fn(s)) {
-			return std::pair<arg_list,bool>(args, false);
+			return std::pair<arg_list,bool>(arg_list(), false);
 		}
 	}
 	
-	return std::pair<arg_list,bool>(args, true);
+	return std::pair<arg_list,bool>(remaining_args, true);
 }
 
 void cmdline_option::print_options(cmdline_option *options)
