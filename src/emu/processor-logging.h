3781efbaf17695ec4bf4301d0d6ed9edf0a33448
Michael Clark <michael@metaparadigm.com>
2017-02-23 14:03:25 +1300

    Fix whitespace

--- src/emu/processor-logging.h
+++ src/emu/processor-logging.h
@@ -18,8 +18,8 @@ namespace riscv {
 		proc_log_csr_smode =       1<<5,       /* Log supervisor status and control registers */
 		proc_log_int_reg =         1<<6,       /* Log integer registers */
 		proc_log_trap =            1<<7,       /* Log processor traps */
-		proc_log_pagewalk =        1<<8,      /* Log virtual memory page walks */
-		proc_log_config =          1<<9,      /* Log config string */
+		proc_log_pagewalk =        1<<8,       /* Log virtual memory page walks */
+		proc_log_config =          1<<9,       /* Log config string */
 		proc_log_ebreak_cli =      1<<10,      /* Switch to debug CLI on ebreak */
 		proc_log_trap_cli =        1<<11,      /* Switch to debug CLI on trap */
 		proc_log_no_pseudo =       1<<12       /* Don't decode pseudoinstructions */
