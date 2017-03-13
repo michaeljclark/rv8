0df7b1cde20781dd86a65ecf37bf9038d62a4082
Michael Clark <michael@metaparadigm.com>
2017-03-14 10:03:17 +1300

    Rename test-loop to test-loop-2 and add test-loop-1 test case

---
+++ src/test/test-loop-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+long arr[] = {
+	1, 2, 3,
+	4, 5, 6,
+	7, 8, 9,
+	10, 11, 12,
+	13, 14, 15,
+	16, 17, 18,
+	19, 20, 21,
+	22, 23, 24,
+	0
+};
+
+int main()
+{
+	long total = 0;
+	for (size_t j = 0; j < (sizeof(arr)/sizeof(long)); j++) {
+		total += arr[j];
+	}
+	printf("total=%ld\n", total);
+}
