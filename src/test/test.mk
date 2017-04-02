aab5c617ff9834546203d4e128901e73d9892097
Michael Clark <michael@metaparadigm.com>
2017-04-02 16:35:58 +1200

    Add test-nbody from The Computer Language Benchmark Game

--- src/test/test.mk
+++ src/test/test.mk
@@ -79,6 +79,7 @@ PROGRAMS = \
 	$(BIN_DIR)/test-loop-1 \
 	$(BIN_DIR)/test-loop-2 \
 	$(BIN_DIR)/test-open \
+	$(BIN_DIR)/test-nbody \
 	$(BIN_DIR)/test-sha512 \
 	$(BIN_DIR)/test-fpu-gen \
 	$(BIN_DIR)/test-fpu-printf \
@@ -158,6 +159,9 @@ $(BIN_DIR)/test-loop-2: $(OBJ_DIR)/test-loop-2.o ; $(CC) $(CFLAGS) $^ -o $@
 
 $(OBJ_DIR)/test-open.o: $(SRC_DIR)/test-open.c ; $(CC) $(CFLAGS) -c $^ -o $@
 $(BIN_DIR)/test-open: $(OBJ_DIR)/test-open.o ; $(CC) $(CFLAGS) $^ -o $@
+
+$(OBJ_DIR)/test-nbody.o: $(SRC_DIR)/test-nbody.c ; $(CC) $(CFLAGS) -c $^ -o $@
+$(BIN_DIR)/test-nbody: $(OBJ_DIR)/test-nbody.o ; $(CC) $(CFLAGS) $^ -lm -o $@
 
 $(OBJ_DIR)/test-sha512.o: $(SRC_DIR)/test-sha512.c ; $(CC) $(CFLAGS) -c $^ -o $@
 $(BIN_DIR)/test-sha512: $(OBJ_DIR)/test-sha512.o ; $(CC) $(CFLAGS) $^ -o $@
