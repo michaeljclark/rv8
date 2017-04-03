8ad857ff33b158cd0eaba73d42a655adc308de36
Michael Clark <michael@metaparadigm.com>
2017-04-04 11:58:51 +1200

    Add text to test-malloc indicating malloc is broken

--- src/test/test-malloc.c
+++ src/test/test-malloc.c
@@ -8,10 +8,15 @@ int main()
 {
 	for (size_t i = 0; i < MALLOC_ITERS; i++) {
 		char *b = (char*)malloc(MALLOC_SIZE);
+		if (!b) {
+			printf("malloc appears to be broken\n");
+			exit(9);
+		}
 		for (size_t j = 0; j < MALLOC_SIZE; j++) {
 			b[j] = 0;
 		}
 		free(b);
 	}
 	printf("malloc appears to work\n");
+	exit(0);
 }
