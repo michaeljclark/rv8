9a73adb36938fbe149b4f8ccfd01388033f0b61b
Michael Clark <michaeljclark@mac.com>
2017-07-12 10:44:24 +1200

    Build benchmark programs for host and target

--- src/test/test.mk
+++ src/test/test.mk
@@ -8,7 +8,6 @@
 # host
 OS := $(shell uname -s | sed 's/ /_/' | tr A-Z a-z)
 CPU := $(shell uname -m | sed 's/ /_/' | tr A-Z a-z)
-BIN = build/$(OS)_$(CPU)/bin
 
 # target
 CC = ${RISCV}/bin/${TARGET}-gcc
@@ -26,6 +25,9 @@ OBJ_DIR = build/$(TARGET_DIR)/obj
 BIN_DIR = build/$(TARGET_DIR)/bin
 GEN_DIR = build/$(TARGET_DIR)/gen
 
+HOST_BIN_DIR = build/$(OS)_$(CPU)/test
+HOST_OBJ_DIR = build/$(OS)_$(CPU)/obj
+
 # compiler function tests
 check_opt = $(shell T=$$(mktemp /tmp/test.XXXX); echo 'int main() { return 0; }' > $$T.$(2) ; $(1) $(3) $$T.$(2) -o /dev/null >/dev/null 2>&1 ; echo $$?; rm $$T $$T.$(2))
 
@@ -38,7 +40,6 @@ SPIKE = ${RISCV}/bin/spike --isa=$(ARCH) ${PK}
 endif
 
 CFLAGS = -Os -g -march=$(ARCH) -Wall -fpie -ffunction-sections -fdata-sections
-CXXFLAGS = -std=c++1y $(CFLAGS)
 
 ifeq ($(EMULATOR),)
 EMULATOR = $(SPIKE)
@@ -58,8 +59,13 @@ PROGRAMS = \
 	$(BIN_DIR)/hello-world-pcrel-nano \
 	$(BIN_DIR)/hello-world-pcrel-pico \
 	$(BIN_DIR)/test-aes \
-	$(BIN_DIR)/test-args \
 	$(BIN_DIR)/test-dhrystone \
+	$(BIN_DIR)/test-miniz \
+	$(BIN_DIR)/test-norx \
+	$(BIN_DIR)/test-sha512 \
+	$(BIN_DIR)/test-primes \
+	$(BIN_DIR)/test-qsort \
+	$(BIN_DIR)/test-args \
 	$(BIN_DIR)/test-loop-1 \
 	$(BIN_DIR)/test-loop-2 \
 	$(BIN_DIR)/test-fusion-elision \
@@ -68,12 +74,7 @@ PROGRAMS = \
 	$(BIN_DIR)/test-fusion-vanilla \
 	$(BIN_DIR)/test-open \
 	$(BIN_DIR)/test-malloc \
-	$(BIN_DIR)/test-miniz \
 	$(BIN_DIR)/test-nbody \
-	$(BIN_DIR)/test-norx \
-	$(BIN_DIR)/test-primes \
-	$(BIN_DIR)/test-qsort \
-	$(BIN_DIR)/test-sha512 \
 	$(BIN_DIR)/test-fpu-gen \
 	$(BIN_DIR)/test-fpu-printf \
 	$(BIN_DIR)/test-infinite-loop \
@@ -85,6 +86,15 @@ PROGRAMS = \
 	$(BIN_DIR)/test-reloc-imm \
 	$(BIN_DIR)/test-sbi-info \
 	$(BIN_DIR)/test-sbi-timer
+
+HOST_PROGRAMS = \
+	$(HOST_BIN_DIR)/test-aes \
+	$(HOST_BIN_DIR)/test-dhrystone \
+	$(HOST_BIN_DIR)/test-miniz \
+	$(HOST_BIN_DIR)/test-norx \
+	$(HOST_BIN_DIR)/test-sha512 \
+	$(HOST_BIN_DIR)/test-primes \
+	$(HOST_BIN_DIR)/test-qsort
 
 ifeq ($(TARGET),riscv64-unknown-elf)
 PROGRAMS += \
@@ -99,9 +109,9 @@ PROGRAMS += \
 	$(BIN_DIR)/test-m-sv39
 endif
 
-all: dirs $(ASSEMBLY) $(PROGRAMS)
+all: dirs $(ASSEMBLY) $(PROGRAMS) $(HOST_PROGRAMS)
 
-dirs: ; @mkdir -p $(ASM_DIR) $(OBJ_DIR) $(BIN_DIR) $(GEN_DIR)
+dirs: ; @mkdir -p $(ASM_DIR) $(OBJ_DIR) $(BIN_DIR) $(GEN_DIR) $(HOST_BIN_DIR) $(HOST_OBJ_DIR)
 
 clean:
 	rm -f $(PROGRAMS) *.o
@@ -123,6 +133,57 @@ test-sys: all
 	$(EMULATOR) $(BIN_DIR)/test-m-mmio-timer
 	$(EMULATOR) $(BIN_DIR)/test-m-sv39
 
+# host benchmarks
+
+$(HOST_OBJ_DIR)/test-aes.o: $(SRC_DIR)/test-aes.c ; cc -O3 -c $^ -o $@
+$(HOST_BIN_DIR)/test-aes: $(HOST_OBJ_DIR)/test-aes.o ; cc -O3 $^ -o $@
+
+$(HOST_OBJ_DIR)/test-dhrystone.o: $(SRC_DIR)/test-dhrystone.c ; cc -O3 -DTIME -c $^ -o $@
+$(HOST_BIN_DIR)/test-dhrystone: $(HOST_OBJ_DIR)/test-dhrystone.o ; cc -O3 $^ -o $@
+
+$(HOST_OBJ_DIR)/test-miniz.o: $(SRC_DIR)/test-miniz.c ; cc -O3 -c $^ -o $@
+$(HOST_BIN_DIR)/test-miniz: $(HOST_OBJ_DIR)/test-miniz.o ; cc -O3 $^ -lm -o $@
+
+$(HOST_OBJ_DIR)/test-norx.o: $(SRC_DIR)/test-norx.c ; cc -O3 -c $^ -o $@
+$(HOST_BIN_DIR)/test-norx: $(HOST_OBJ_DIR)/test-norx.o ; cc -O3 $^ -lm -o $@
+
+$(HOST_OBJ_DIR)/test-sha512.o: $(SRC_DIR)/test-sha512.c ; cc -O3 -c $^ -o $@
+$(HOST_BIN_DIR)/test-sha512: $(HOST_OBJ_DIR)/test-sha512.o ; cc -O3 $^ -o $@
+
+$(HOST_OBJ_DIR)/test-primes.o: $(SRC_DIR)/test-primes.c ; cc -O3 -c $^ -o $@
+$(HOST_BIN_DIR)/test-primes: $(HOST_OBJ_DIR)/test-primes.o ; cc -O3 $^ -o $@
+
+$(HOST_OBJ_DIR)/test-qsort.o: $(SRC_DIR)/test-qsort.c ; cc -O3 -c $^ -o $@
+$(HOST_BIN_DIR)/test-qsort: $(HOST_OBJ_DIR)/test-qsort.o ; cc -O3 $^ -o $@
+
+$(HOST_OBJ_DIR)/test-args.o: $(SRC_DIR)/test-args.c ; cc -O3 -c $^ -o $@
+$(HOST_BIN_DIR)/test-args: $(HOST_OBJ_DIR)/test-args.o ; cc -O3 $^ -o $@
+
+# target benchmarks
+
+$(OBJ_DIR)/test-aes.o: $(SRC_DIR)/test-aes.c ; $(CC) $(CFLAGS) -c $^ -o $@
+$(BIN_DIR)/test-aes: $(OBJ_DIR)/test-aes.o ; $(CC) $(CFLAGS) $^ -o $@
+
+$(OBJ_DIR)/test-dhrystone.o: $(SRC_DIR)/test-dhrystone.c ; $(CC) $(CFLAGS) -DTIME -c $^ -o $@
+$(BIN_DIR)/test-dhrystone: $(OBJ_DIR)/test-dhrystone.o ; $(CC) $(CFLAGS) $^ -o $@
+
+$(OBJ_DIR)/test-miniz.o: $(SRC_DIR)/test-miniz.c ; $(CC) $(CFLAGS) -c $^ -o $@
+$(BIN_DIR)/test-miniz: $(OBJ_DIR)/test-miniz.o ; $(CC) $(CFLAGS) $^ -lm -o $@
+
+$(OBJ_DIR)/test-norx.o: $(SRC_DIR)/test-norx.c ; $(CC) $(CFLAGS) -c $^ -o $@
+$(BIN_DIR)/test-norx: $(OBJ_DIR)/test-norx.o ; $(CC) $(CFLAGS) $^ -lm -o $@
+
+$(OBJ_DIR)/test-sha512.o: $(SRC_DIR)/test-sha512.c ; $(CC) $(CFLAGS) -c $^ -o $@
+$(BIN_DIR)/test-sha512: $(OBJ_DIR)/test-sha512.o ; $(CC) $(CFLAGS) $^ -o $@
+
+$(OBJ_DIR)/test-primes.o: $(SRC_DIR)/test-primes.c ; $(CC) $(CFLAGS) -c $^ -o $@
+$(BIN_DIR)/test-primes: $(OBJ_DIR)/test-primes.o ; $(CC) $(CFLAGS) $^ -o $@
+
+$(OBJ_DIR)/test-qsort.o: $(SRC_DIR)/test-qsort.c ; $(CC) $(CFLAGS) -c $^ -o $@
+$(BIN_DIR)/test-qsort: $(OBJ_DIR)/test-qsort.o ; $(CC) $(CFLAGS) $^ -o $@
+
+# target test programs
+
 $(ASM_DIR)/hello-world-libc.s: $(SRC_DIR)/hello-world-libc.c ; $(CC) $(CFLAGS) -S -nostartfiles $^ -o $@
 $(OBJ_DIR)/hello-world-libc.o: $(SRC_DIR)/hello-world-libc.c ; $(CC) $(CFLAGS) -c $^ -o $@
 $(BIN_DIR)/hello-world-libc: $(OBJ_DIR)/hello-world-libc.o ; $(CC) $(CFLAGS) $^ -o $@
@@ -141,17 +202,11 @@ $(BIN_DIR)/hello-world-pcrel-pico: $(OBJ_DIR)/hello-world-pcrel.o ; $(LD) --nmag
 $(OBJ_DIR)/bswap32.o: $(SRC_DIR)/bswap32.s ; $(CC) $(CFLAGS) -c $^ -o $@
 $(OBJ_DIR)/bswap64.o: $(SRC_DIR)/bswap64.s ; $(CC) $(CFLAGS) -c $^ -o $@
 
-$(OBJ_DIR)/test-aes.o: $(SRC_DIR)/test-aes.c ; $(CC) $(CFLAGS) -c $^ -o $@
-$(BIN_DIR)/test-aes: $(OBJ_DIR)/test-aes.o ; $(CC) $(CFLAGS) $^ -o $@
-
 $(OBJ_DIR)/test-args.o: $(SRC_DIR)/test-args.c ; $(CC) $(CFLAGS) -c $^ -o $@
 $(BIN_DIR)/test-args: $(OBJ_DIR)/test-args.o ; $(CC) $(CFLAGS) $^ -o $@
 
 $(OBJ_DIR)/test-bswap.o: $(SRC_DIR)/test-bswap.c ; $(CC) $(CFLAGS) -c $^ -o $@
 $(BIN_DIR)/test-bswap: $(OBJ_DIR)/test-bswap.o $(OBJ_DIR)/bswap32.o $(OBJ_DIR)/bswap64.o ; $(CC) $(CFLAGS) $^ -o $@
-
-$(OBJ_DIR)/test-dhrystone.o: $(SRC_DIR)/test-dhrystone.c ; $(CC) $(CFLAGS) -DTIME -c $^ -o $@
-$(BIN_DIR)/test-dhrystone: $(OBJ_DIR)/test-dhrystone.o ; $(CC) $(CFLAGS) $^ -o $@
 
 $(OBJ_DIR)/test-loop-1.o: $(SRC_DIR)/test-loop-1.c ; $(CC) $(CFLAGS) -c $^ -o $@
 $(BIN_DIR)/test-loop-1: $(OBJ_DIR)/test-loop-1.o ; $(CC) $(CFLAGS) $^ -o $@
@@ -177,23 +232,8 @@ $(BIN_DIR)/test-open: $(OBJ_DIR)/test-open.o ; $(CC) $(CFLAGS) $^ -o $@
 $(OBJ_DIR)/test-malloc.o: $(SRC_DIR)/test-malloc.c ; $(CC) $(CFLAGS) -c $^ -o $@
 $(BIN_DIR)/test-malloc: $(OBJ_DIR)/test-malloc.o ; $(CC) $(CFLAGS) $^ -lm -o $@
 
-$(OBJ_DIR)/test-miniz.o: $(SRC_DIR)/test-miniz.c ; $(CC) $(CFLAGS) -c $^ -o $@
-$(BIN_DIR)/test-miniz: $(OBJ_DIR)/test-miniz.o ; $(CC) $(CFLAGS) $^ -lm -o $@
-
 $(OBJ_DIR)/test-nbody.o: $(SRC_DIR)/test-nbody.c ; $(CC) $(CFLAGS) -c $^ -o $@
 $(BIN_DIR)/test-nbody: $(OBJ_DIR)/test-nbody.o ; $(CC) $(CFLAGS) $^ -lm -o $@
-
-$(OBJ_DIR)/test-norx.o: $(SRC_DIR)/test-norx.c ; $(CC) $(CFLAGS) -c $^ -o $@
-$(BIN_DIR)/test-norx: $(OBJ_DIR)/test-norx.o ; $(CC) $(CFLAGS) $^ -lm -o $@
-
-$(OBJ_DIR)/test-primes.o: $(SRC_DIR)/test-primes.c ; $(CC) $(CFLAGS) -c $^ -o $@
-$(BIN_DIR)/test-primes: $(OBJ_DIR)/test-primes.o ; $(CC) $(CFLAGS) $^ -o $@
-
-$(OBJ_DIR)/test-qsort.o: $(SRC_DIR)/test-qsort.c ; $(CC) $(CFLAGS) -c $^ -o $@
-$(BIN_DIR)/test-qsort: $(OBJ_DIR)/test-qsort.o ; $(CC) $(CFLAGS) $^ -o $@
-
-$(OBJ_DIR)/test-sha512.o: $(SRC_DIR)/test-sha512.c ; $(CC) $(CFLAGS) -c $^ -o $@
-$(BIN_DIR)/test-sha512: $(OBJ_DIR)/test-sha512.o ; $(CC) $(CFLAGS) $^ -o $@
 
 $(OBJ_DIR)/test-fpu-gen.o: $(SRC_DIR)/test-fpu-gen.c ; $(CC) $(CFLAGS) -c $^ -o $@
 $(BIN_DIR)/test-fpu-gen: $(OBJ_DIR)/test-fpu-gen.o ; $(CC) $(CFLAGS) $^ -o $@
