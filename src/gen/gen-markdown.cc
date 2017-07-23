3edc55261c43dc848d7ab20d63d7a509b768b4d2
Michael Clark <michaeljclark@mac.com>
2017-07-24 11:19:56 +1200

    Use HTML <sub> to reduce size of markdown instruction tables

--- src/gen/gen-markdown.cc
+++ src/gen/gen-markdown.cc
@@ -31,42 +31,45 @@ std::vector<cmdline_option> rv_gen_markdown::get_cmdline_options()
 
 static void print_markdown(rv_gen *gen)
 {
-	static const char* fmt = "%-25s|%-40s|%s\n";
+	static const char* fmt = "%s | %s | %s\n";
 
 	for (auto &ext : gen->extensions) {
-		// check if this extension is in the selected subset
 		if (ext->opcodes.size() == 0 || (gen->ext_subset.size() > 0 &&
 			std::find(gen->ext_subset.begin(), gen->ext_subset.end(), ext) == gen->ext_subset.end())) {
 			continue;
 		}
 
-		// print heading
 		printf("_**%s**_\n\n", ext->description.c_str());
 		printf(fmt, "Format", "Name", "Pseudocode");
 		printf(fmt, ":--", ":--", ":--");
 
-		// print opcodes
 		for (auto &opcode : ext->opcodes) {
-			// skip psuedo opcode
 			if (opcode->is_pseudo()) continue;
 
 			auto name = opcode->name;
 			auto operand_comps = split(opcode->format->operands, ",");
 			std::transform(name.begin(), name.end(), name.begin(), ::toupper);
 			if (operand_comps.size() == 1 && operand_comps[0] == "none") operand_comps[0] = "";
-			std::string opcode_format;
-			opcode_format.append("`");
-			opcode_format.append(name);
+			std::string format;
+			format.append("<code><sub>");
+			format.append(name);
 			if (operand_comps.size() > 0) {
-				opcode_format.append(" ");
-				opcode_format.append(join(operand_comps, ", "));
+				format.append(" ");
+				format.append(join(operand_comps, ", "));
 			}
-			opcode_format.append("`");
+			format.append("</sub></code>");
 
-			printf(fmt,
-				opcode_format.c_str(),
-				opcode->fullname.c_str(),
-				opcode->pseudocode_alt.c_str());
+			std::string fullname;
+			fullname.append("<sub>");
+			fullname.append(opcode->fullname);
+			fullname.append("</sub>");
+
+			std::string pseudocode;
+			pseudocode.append("<sub>");
+			pseudocode.append(opcode->pseudocode_alt);
+			pseudocode.append("</sub>");
+
+			printf(fmt, format.c_str(), fullname.c_str(), pseudocode.c_str());
 		}
 		printf("\n");
 	}
