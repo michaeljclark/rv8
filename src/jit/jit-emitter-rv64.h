0106c1310f9d6c9db1a8f9cc7f325a4a8b15d881
Michael Clark <michaeljclark@mac.com>
2017-08-07 08:32:07 +1200

    [zext.w] use full width accesses to spill slots and zero extend in register

--- src/jit/jit-emitter-rv64.h
+++ src/jit/jit-emitter-rv64.h
@@ -3777,7 +3777,8 @@ namespace riscv {
 				if (rdx > 0) {
 					as.movzx(x86::gpq(rdx), x86::gpd(rdx));
 				} else {
-					as.mov(x86::eax, rbp_reg_d(dec.rd));
+					as.mov(x86::rax, rbp_reg_q(dec.rd));
+					as.movzx(x86::rax, x86::eax);
 					as.mov(rbp_reg_q(dec.rd), x86::rax);
 				}
 			}
@@ -3785,12 +3786,14 @@ namespace riscv {
 				if (rdx > 0 && rs1x > 0) {
 					as.movzx(x86::gpq(rdx), x86::gpd(rs1x));
 				} else if (rdx > 0) {
-					as.mov(x86::gpd(rdx), rbp_reg_d(dec.rs1));
+					as.mov(x86::gpq(rdx), rbp_reg_q(dec.rs1));
+					as.movzx(x86::gpq(rdx), x86::gpd(rdx));
 				} else if (rs1x > 0) {
 					as.movzx(x86::rax, x86::gpd(rs1x));
 					as.mov(rbp_reg_q(dec.rd), x86::rax);
 				} else {
-					as.movzx(x86::rax, rbp_reg_d(dec.rs1));
+					as.mov(x86::rax, rbp_reg_q(dec.rs1));
+					as.movzx(x86::rax, x86::eax);
 					as.mov(rbp_reg_q(dec.rd), x86::rax);
 				}
 			}
